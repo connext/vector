29e6b7b654857486d03d8ec232877c32c7558430
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-09-05 19:42:59 +0400

    Basic types

--- modules/engine/types.ts
+++ modules/engine/types.ts
@@ -1,12 +1,30 @@
-export type UpdateParams = {
-    channelId: string,
+import { enumify } from "./utils";
 
-}
+export type UpdateParams = {
+  channelId: string;
+  type: UpdateType;
+};
 
 export type ChannelState = {
-
-}
+    channelId: string;
+    participants: string[];
+    chainId: string;
+    latestNonce: string;
+    latestUpdate: ChannelUpdate
+};
 
 export type ChannelUpdate = {
-    
-}
+  counterpartyPublicIdentifier: string;
+  nonce: string;
+  type: UpdateType;
+  commitment: MultisigCommitment;
+};
+
+export const UpdateType = enumify({
+    setup: "setup",
+    deposit: "deposit",
+    withdraw: "withdraw",
+    create: "create",
+    resolve: "resolve",
+});
+export type UpdateType = typeof UpdateType[keyof typeof UpdateType];
