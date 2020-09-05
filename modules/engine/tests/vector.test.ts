c385cd1507262ab1faaee1d2b54ea77cd21517d7
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-09-05 22:07:31 +0400

    mock test -- still need stuff here

---
+++ modules/engine/tests/vector.test.ts
@@ -0,0 +1,26 @@
+import { getRandomChannelSigner } from "@connext/utils";
+
+import { Vector } from "../vector";
+import { memoryMessagingService } from "./testing/services";
+import { MemoryLockService } from "./testing/services/memory-lock-service";
+import { getMemoryStore } from "./testing/services/memory-store";
+
+import { expect } from "./testing/assertions";
+
+describe("Vector", () => {
+  it("is defined", () => {
+    expect(Vector).to.be.ok;
+  });
+
+  it("can be created", async () => {
+    const store = getMemoryStore();
+    await store.init();
+    const node = await Vector.connect(
+      memoryMessagingService,
+      new MemoryLockService(),
+      store,
+      getRandomChannelSigner(),
+    );
+    expect(node).to.be.ok;
+  });
+});
