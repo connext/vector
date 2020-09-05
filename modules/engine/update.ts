d9b556e9f42bb1f33f0ace115076a647d69ac793
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-09-05 19:44:17 +0400

    Update/validate scaffolding

--- modules/engine/update.ts
+++ modules/engine/update.ts
@@ -1,5 +1,33 @@
-import { UpdateParams } from "./types";
+import { UpdateParams, ChannelUpdate, UpdateType } from "./types";
 
-export async function update(params: UpdateParams) {
-    
+export async function generateUpdate(params: UpdateParams, storeService): Promise<ChannelUpdate> {
+    const state = await storeService.getChannelState();
+
+    switch (params.type) {
+        case UpdateType.setup: {
+
+        }
+
+        case UpdateType.deposit: {
+
+        }
+
+        case UpdateType.withdraw: {
+
+        }
+
+        case UpdateType.create: {
+
+        }
+
+        case UpdateType.resolve: {
+
+        }
+
+        default: {
+            throw new Error(`Unexpected UpdateType in params: ${params.type}`);
+        }
+    }
+
+    return {} as ChannelUpdate;
 }
