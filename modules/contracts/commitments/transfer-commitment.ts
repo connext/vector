75b12cc388e81cb59693841399a98b4d35024007
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-09-06 14:55:11 +0400

    Add initial commitment ideas

---
+++ modules/contracts/commitments/transfer-commitment.ts
@@ -0,0 +1,15 @@
+// TODO
+
+// For now, this must be created on every (both) transfer updates.
+
+// Should contain at least the following info:
+// - transferId
+// - merkle proof
+// - transferDefinition
+// - transferState
+
+// transferId should be hash of:
+// - channelId
+// - transferDefinition
+// - transferTimeout
+// - nonceCreatedAt
