72e802fc5c0ff78468e012dfb0e185fc923e1bf1
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-09-09 00:11:07 +0400

    Add ITransferDefinition

---
+++ modules/contracts/interfaces/ITransferDefinition.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.1;
+pragma experimental "ABIEncoderV2";
+
+
+interface ITransferDefinition {
+
+    // Validates the initial state of the transfer.
+    // Called by validator.ts during `create` updates.
+    function initialize(bytes calldata)
+        external
+        view
+        returns (bool)
+
+    // Performs a state transition to resolve a transfer
+    // and returns final balances. Called by validator.ts
+    // during `resolve` updates.
+    function resolve(bytes calldata, bytes calldata)
+        external
+        view
+        returns (bytes memory)
+        
+}
