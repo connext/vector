036be14330c242cc23906aa4e0e92af29d1fff5d
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-09-13 13:14:34 +0200

    Directly use engine

--- modules/node-core/src/index.ts
+++ modules/node-core/src/index.ts
@@ -16,7 +16,6 @@ import {
   ResolveTransferParams,
 } from "@connext/vector-types";
 
-import { setupEngineProvider } from "./engineProvider";
 import {
   convertConditionalTransferParams,
   convertResolveConditionParams,
@@ -27,7 +26,7 @@ export class NodeCore {
   private constructor(
     private readonly messaging: IMessagingService,
     private readonly store: INodeCoreStore,
-    private readonly engineProvider: Vector,
+    private readonly engine: Vector,
     private readonly chainProviders: ChainProviders,
     private readonly chainAddresses: ChainAddresses,
   ) {}
@@ -40,11 +39,8 @@ export class NodeCore {
     chainProviders: ChainProviders,
     chainAddresses: ChainAddresses,
   ): Promise<NodeCore> {
-    // TODO write this
-    const engineProvider = await setupEngineProvider(messaging, lock, store as IEngineStore, signer, chainProviders);
-    // TODO look at what was done for SDK to relay events
-
-    const nodeCore = new NodeCore(messaging, store, engineProvider, chainProviders, chainAddresses);
+    const engine = await Vector.connect(messaging, lock, store as IEngineStore, signer, chainProviders);
+    const nodeCore = new NodeCore(messaging, store, engine, chainProviders, chainAddresses);
     await nodeCore.setupListener();
     return nodeCore;
   }
@@ -53,7 +49,7 @@ export class NodeCore {
 
   public async deposit(params: DepositParams): Promise<any> {
     // TODO we need a deposit response here
-    return this.engineProvider.deposit(params);
+    return this.engine.deposit(params);
   }
 
   public async conditionalTransfer(params: ConditionalTransferParams): Promise<any> {
@@ -62,7 +58,7 @@ export class NodeCore {
 
     // First, get translated `create` params using the passed in conditional transfer ones
     const createParams: CreateTransferParams = await convertConditionalTransferParams(params, this.chainAddresses);
-    return this.engineProvider.createTransfer(createParams);
+    return this.engine.createTransfer(createParams);
   }
 
   public async resolveCondition(params: ResolveConditionParams): Promise<any> {
@@ -71,7 +67,7 @@ export class NodeCore {
 
     // First, get translated `resolve` params using the passed in resolve condition ones
     const resolveParams: ResolveTransferParams = await convertResolveConditionParams(params);
-    return this.engineProvider.resolveTransfer(resolveParams);
+    return this.engine.resolveTransfer(resolveParams);
   }
 
   public async withdraw(params: WithdrawParams): Promise<any> {
@@ -79,7 +75,7 @@ export class NodeCore {
     // TODO input validation
 
     const withdrawParams: CreateTransferParams = await convertWithdrawParams(params, this.chainAddresses);
-    return this.engineProvider.createTransfer(withdrawParams);
+    return this.engine.createTransfer(withdrawParams);
   }
 
   public async transfer(params: TransferParams): Promise<any> {
