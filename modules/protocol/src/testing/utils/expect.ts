6f72850ed35d85a2d25e03d4f48de7707cd7ee0a
LayneHaber <layne.haber@gmail.com>
2020-09-17 14:04:48 -0600

    user BN

--- modules/protocol/src/testing/utils/expect.ts
+++ modules/protocol/src/testing/utils/expect.ts
@@ -1,8 +1,10 @@
 import { use } from "chai";
 import promised from "chai-as-promised";
 import subset from "chai-subset";
+import { solidity } from "ethereum-waffle";
 
 use(subset);
 use(promised);
+use(solidity);
 
 export { expect } from "chai";
