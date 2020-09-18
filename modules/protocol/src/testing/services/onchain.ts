4ebbb9ed8e5fe0f3f3d39ef7c4f2f9e7574b9770
LayneHaber <layne.haber@gmail.com>
2020-09-18 12:25:17 -0600

    WIP mock, initial draft

--- modules/protocol/src/testing/services/onchain.ts
+++ modules/protocol/src/testing/services/onchain.ts
@@ -1,23 +1,57 @@
+import { VectorOnchainTransactionService } from "@connext/vector-contracts";
 import { IVectorOnchainTransactionService, Result } from "@connext/vector-types";
+import { mkHash } from "@connext/vector-utils";
 import { BigNumber } from "ethers";
+import Sinon from "sinon";
+
+type StubType = {
+  [K in keyof IVectorOnchainTransactionService]: IVectorOnchainTransactionService[K];
+};
 
 export class MockOnchainTransactionService implements IVectorOnchainTransactionService {
+  public readonly stubs: StubType;
+
+  constructor(overrides: Partial<StubType> = {}) {
+    this.stubs = Sinon.createStubInstance(VectorOnchainTransactionService, {
+      getChannelOnchainBalance: Result.ok<BigNumber>(BigNumber.from(10)) as any,
+
+      getLatestDepositByAssetId: Result.ok<{ nonce: BigNumber; amount: BigNumber }>({
+        nonce: BigNumber.from(7),
+        amount: BigNumber.from(14),
+      }) as any,
+
+      getChannelFactoryBytecode: Result.ok<string>(mkHash("0x51523ase")) as any,
+
+      ...overrides,
+    });
+  }
+
   getChannelOnchainBalance(
     channelAddress: string,
     chainId: number,
     assetId: string,
   ): Promise<Result<BigNumber, Error>> {
-    throw new Error("Method not implemented.");
+    return this.stubs["getChannelOnchainBalance"](channelAddress, chainId, assetId);
   }
+
   getLatestDepositByAssetId(
     channelAddress: string,
     chainId: number,
     assetId: string,
     latestDepositNonce: number,
   ): Promise<Result<{ nonce: BigNumber; amount: BigNumber }, Error>> {
-    throw new Error("Method not implemented.");
+    return this.stubs["getLatestDepositByAssetId"](channelAddress, chainId, assetId, latestDepositNonce);
   }
+
   getChannelFactoryBytecode(channelFactoryAddress: string, chainId: number): Promise<Result<string, Error>> {
-    throw new Error("Method not implemented.");
+    return this.stubs["getChannelFactoryBytecode"](channelFactoryAddress, chainId);
+  }
+
+  // Easy method to set stubs mid-test
+  setStub(
+    method: keyof IVectorOnchainTransactionService,
+    ret: StubType[typeof method],
+  ): void {
+    this.stubs[method] = Sinon.stub().resolves(ret);
   }
 }
