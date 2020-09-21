cc9fa43f6e39793f55e9142e1bfc38a33ab4a942
LayneHaber <layne.haber@gmail.com>
2020-09-21 00:08:44 -0600

    Use mocks in utils unit tests

--- modules/protocol/src/testing/utils.spec.ts
+++ modules/protocol/src/testing/utils.spec.ts
@@ -1,196 +1,144 @@
-import { BigNumber, Contract, constants, ContractFactory } from "ethers";
-import { VectorChannel, TestToken } from "@connext/vector-contracts";
-import { JsonRpcProvider, Balance, CoreChannelState, IChannelSigner } from "@connext/vector-types";
-import { createTestChannelState, getRandomChannelSigner } from "@connext/vector-utils";
+import { BigNumber, constants, BigNumberish } from "ethers";
+import { Balance, Result } from "@connext/vector-types";
+import { mkAddress } from "@connext/vector-utils";
 import { expect } from "chai";
+import Sinon from "sinon";
 
 import { reconcileDeposit } from "../utils";
 
 import { env } from "./utils";
-import { MockOnchainServce } from "./services/onchain";
+import { MockOnchainService, MockOnchainStubType } from "./services/onchain";
+
+type ReconcileDepositTest = {
+  initialBalance: Omit<Balance, "to">;
+  latestDepositNonce: number;
+  lockedBalance: string;
+  assetId: string;
+  aliceDeposit: BigNumberish; // depositA deposit
+  bobDeposit: BigNumberish; // user deposit
+  stubs: Partial<MockOnchainStubType>;
+  expected: Omit<Balance, "to"> & { latestDepositNonce: number };
+  error: Error;
+};
 
 describe("utils", () => {
   describe("reconcileDeposit", () => {
+    const channelAddress = mkAddress("0xccc");
     const chainId = parseInt(Object.keys(env.chainProviders)[0]);
-    const providerUrl = env.chainProviders[chainId];
-    const provider = new JsonRpcProvider(providerUrl);
-    const wallet = env.sugarDaddy.connect(provider);
-    let state: CoreChannelState;
-    let channelContract: Contract;
-    let token: Contract;
-    let signer: IChannelSigner;
+    const to = [mkAddress("0xaaa"), mkAddress("0xbbb")];
 
-    async function depositA(amount: string, assetId: string): Promise<void> {
-      if (assetId === constants.AddressZero) {
-        await channelContract.functions.depositA(assetId, BigNumber.from(amount), { value: BigNumber.from(amount) });
-      } else {
-        await token.approve(channelContract.address, BigNumber.from(amount));
-        await channelContract.functions.depositA(assetId, BigNumber.from(amount));
-      }
-    }
+    const getOnchainService = (testParams: Partial<ReconcileDepositTest>) => {
+      const { initialBalance, latestDepositNonce, stubs, aliceDeposit, bobDeposit } = testParams;
+      const initialChainBalance = (initialBalance?.amount ?? []).reduce(
+        (prev, curr) => prev.add(curr),
+        BigNumber.from(0),
+      );
 
-    async function depositB(amount: string, assetId: string): Promise<void> {
-      if (assetId === constants.AddressZero) {
-        await wallet.sendTransaction({ to: state.channelAddress, value: BigNumber.from(amount) });
-      } else {
-        await token.transfer(channelContract.address, BigNumber.from(amount));
-      }
-    }
+      // Create the stubs
+      const defaults = {
+        // Default the value onchain + depositA + multisig deposit
+        getChannelOnchainBalance: Result.ok<BigNumber>(initialChainBalance.add(aliceDeposit ?? 0).add(bobDeposit ?? 0)),
 
-    async function validateRet(
-      ret: { balance: Balance; latestDepositNonce: number },
-      assetId: string,
-      amount: string[],
-      initialBalance: Balance,
-    ): Promise<void> {
-      let onchainDepositA: { nonce: BigNumber; amount: BigNumber };
-      try {
-        onchainDepositA = await channelContract.latestDepositByAssetId(assetId);
-      } catch (e) {
-        // Channel contract was not deployed, use 0 value
-        onchainDepositA = { amount: BigNumber.from(0), nonce: BigNumber.from(0) };
-      }
-      const expectedBalance = {
-        ...initialBalance,
-        amount: [
-          BigNumber.from(initialBalance.amount[0]).add(amount[0]).toString(),
-          BigNumber.from(initialBalance.amount[1]).add(amount[1]).toString(),
-        ],
+        // Default is nonce 1, deposit 0
+        getLatestDepositByAssetId: Result.ok<{ nonce: BigNumber; amount: BigNumber }>({
+          nonce: BigNumber.from((latestDepositNonce ?? 0) + 1),
+          amount: BigNumber.from(aliceDeposit ?? 0),
+        }),
+
+        ...stubs,
       };
-      expect(expectedBalance).deep.eq(ret.balance);
-      expect(onchainDepositA.nonce.toNumber()).to.eq(ret.latestDepositNonce);
-    }
 
-    beforeEach(async () => {
-      // TODO replace this with a mock
-      channelContract = await new ContractFactory(VectorChannel.abi, VectorChannel.bytecode, wallet).deploy();
-      await channelContract.deployed();
-      token = new Contract(env.chainAddresses[chainId].TestToken.address, TestToken.abi, wallet);
-      state = createTestChannelState("setup", {
-        assetIds: [constants.AddressZero, token.address],
+      // Create the onchain service
+      const onchain = new MockOnchainService();
+      Object.entries(defaults).forEach(([key, value]) => {
+        onchain.setStub(key as any, value as any);
+      });
+
+      // Return the onchain service
+      return onchain;
+    };
+
+    afterEach(() => {
+      // Restore all mocks from the onchain service
+      Sinon.restore();
+    });
+
+    const tests: (Partial<ReconcileDepositTest> & { name: string })[] = [
+      {
+        name: "should work for Alice Eth deposit when onchain deposit was succuessful",
+        aliceDeposit: 15,
+        initialBalance: { amount: ["3", "9"] },
         latestDepositNonce: 0,
-        channelAddress: channelContract.address,
+        expected: { amount: ["18", "9"], latestDepositNonce: 1 },
+      },
+      {
+        name: "should work for Alice Token deposit when onchain deposit was successful",
+        aliceDeposit: 15,
+        initialBalance: { amount: ["3", "9"] },
+        latestDepositNonce: 0,
+        assetId: mkAddress("0xdddd"),
+        expected: { amount: ["18", "9"], latestDepositNonce: 1 },
+      },
+      {
+        name: "should work for Bob Eth deposit when onchain deposit was successful",
+        bobDeposit: 7,
+        initialBalance: { amount: ["3", "9"] },
+        expected: { amount: ["3", "16"], latestDepositNonce: 1 },
+      },
+      {
+        name: "should work for Bob Token deposit when onchain deposit was successful",
+        bobDeposit: 7,
+        initialBalance: { amount: ["3", "9"] },
+        assetId: mkAddress("0xdddd"),
+        expected: { amount: ["3", "16"], latestDepositNonce: 1 },
+      },
+      {
+        name: "should work for both Eth deposit when onchain deposits were successful",
+        bobDeposit: 7,
+        aliceDeposit: 15,
+        initialBalance: { amount: ["3", "9"] },
+        latestDepositNonce: 0,
+        expected: { amount: ["18", "16"], latestDepositNonce: 1 },
+      },
+      {
+        name: "should work for both token deposit when onchain deposits were successful",
+        bobDeposit: 7,
+        aliceDeposit: 15,
+        initialBalance: { amount: ["3", "9"] },
+        latestDepositNonce: 0,
+        assetId: mkAddress("0xdddd"),
+        expected: { amount: ["18", "16"], latestDepositNonce: 1 },
+      },
+    ];
+
+    for (const test of tests) {
+      const { name, initialBalance, latestDepositNonce, lockedBalance, assetId, error, expected } = test;
+      it(name, async () => {
+        // Create the onchain service
+        const chainService = getOnchainService(test);
+
+        // Run the test
+        const result = await reconcileDeposit(
+          channelAddress,
+          chainId,
+          { ...(initialBalance ?? { amount: ["0", "0"] }), to },
+          latestDepositNonce ?? 0,
+          lockedBalance ?? "0",
+          assetId ?? constants.AddressZero,
+          chainService,
+        );
+
+        if (error) {
+          expect(result.getError()).to.be.eq(error);
+        } else if (expected) {
+          expect(result.getError()).to.be.undefined;
+          const returned = result.getValue()!;
+          expect(returned).to.containSubset({
+            balance: { amount: expected.amount, to },
+            latestDepositNonce: expected.latestDepositNonce,
+          });
+        }
       });
-
-      // Test channel state starts with some eth and tokens, deposit them now
-      await wallet.sendTransaction({
-        to: channelContract.address,
-        value: BigNumber.from(state.balances[0].amount[0]).add(state.balances[0].amount[1]).add(state.lockedBalance[0]),
-      });
-      await token.transfer(
-        channelContract.address,
-        BigNumber.from(state.balances[1].amount[0]).add(state.balances[1].amount[1]).add(state.lockedBalance[1]),
-      );
-      signer = getRandomChannelSigner(provider);
-    });
-
-    it("should work for Alice Eth deposit", async () => {
-      const assetId = constants.AddressZero;
-      const amount = ["7", "0"];
-
-      await depositA(amount[0], assetId);
-      const ret = await reconcileDeposit(
-        state.channelAddress,
-        chainId,
-        state.balances[0],
-        state.latestDepositNonce,
-        state.lockedBalance[0],
-        assetId,
-        new MockOnchainServce(),
-      );
-
-      await validateRet(ret.getValue(), assetId, amount, state.balances[0]);
-    });
-
-    it("should work for Alice Token deposit", async () => {
-      const assetId = token.address;
-      const amount = ["7", "0"];
-
-      await depositA(amount[0], assetId);
-      const ret = await reconcileDeposit(
-        state.channelAddress,
-        chainId,
-        state.balances[1],
-        state.latestDepositNonce,
-        state.lockedBalance[1],
-        assetId,
-        new MockOnchainServce(),
-      );
-
-      await validateRet(ret.getValue(), assetId, amount, state.balances[1]);
-    });
-
-    it("should work for Bob Eth deposit", async () => {
-      const assetId = constants.AddressZero;
-      const amount = ["0", "7"];
-
-      await depositB(amount[1], assetId);
-      const ret = await reconcileDeposit(
-        state.channelAddress,
-        chainId,
-        state.balances[0],
-        state.latestDepositNonce,
-        state.lockedBalance[0],
-        assetId,
-        new MockOnchainServce(),
-      );
-      await validateRet(ret.getValue(), assetId, amount, state.balances[0]);
-    });
-
-    it("should work for Bob Token deposit", async () => {
-      const assetId = token.address;
-      const amount = ["0", "7"];
-
-      await depositB(amount[1], assetId);
-      const ret = await reconcileDeposit(
-        state.channelAddress,
-        chainId,
-        state.balances[1],
-        state.latestDepositNonce,
-        state.lockedBalance[1],
-        assetId,
-        new MockOnchainServce(),
-      );
-
-      await validateRet(ret.getValue(), assetId, amount, state.balances[1]);
-    });
-
-    it("should work for both Eth deposit", async () => {
-      const assetId = constants.AddressZero;
-      const amount = ["7", "5"];
-
-      await depositA(amount[0], assetId);
-      await depositB(amount[1], assetId);
-      const ret = await reconcileDeposit(
-        state.channelAddress,
-        chainId,
-        state.balances[0],
-        state.latestDepositNonce,
-        state.lockedBalance[0],
-        assetId,
-        new MockOnchainServce(),
-      );
-
-      await validateRet(ret.getValue(), assetId, amount, state.balances[0]);
-    });
-
-    it("should work for both token deposit", async () => {
-      const assetId = token.address;
-      const amount = ["7", "5"];
-
-      await depositA(amount[0], assetId);
-      await depositB(amount[1], assetId);
-      const ret = await reconcileDeposit(
-        state.channelAddress,
-        chainId,
-        state.balances[1],
-        state.latestDepositNonce,
-        state.lockedBalance[1],
-        assetId,
-        new MockOnchainServce(),
-      );
-
-      await validateRet(ret.getValue(), assetId, amount, state.balances[1]);
-    });
+    }
   });
 });
