e22e204cfa201a470eb7bfe065c1dd1c2ab8579e
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-09-23 18:17:46 +0200

    Merge the onchain services

--- modules/contracts/src.ts/test/utils.ts
+++ modules/contracts/src.ts/test/utils.ts
@@ -3,6 +3,7 @@ import { use } from "chai";
 import promised from "chai-as-promised";
 import subset from "chai-subset";
 import { MockProvider, solidity } from "ethereum-waffle";
+import pino, { BaseLogger } from "pino";
 
 import { VectorOnchainService } from "../onchainService";
 
@@ -14,8 +15,11 @@ export const expect = use(solidity).expect;
 
 export const provider = waffle.provider;
 
-export const getOnchainTxService = async (provider: MockProvider): Promise<VectorOnchainService> => {
+export const getOnchainTxService = async (
+  provider: MockProvider,
+  log: BaseLogger = pino(),
+): Promise<VectorOnchainService> => {
   const network = await provider.getNetwork();
   const chainProviders = { [network.chainId]: provider };
-  return new VectorOnchainService(chainProviders);
+  return new VectorOnchainService(chainProviders, log.child({ module: "VectorOnchainService" }));
 };
