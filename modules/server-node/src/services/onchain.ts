d42496664c98e61723c414d2356a7e35e3e8777f
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-09-24 16:13:42 +0200

    ♻️ Refactor onchain services

--- modules/server-node/src/services/onchain.ts
+++ modules/server-node/src/services/onchain.ts
@@ -6,7 +6,7 @@ import {
   Result,
   ERC20Abi,
 } from "@connext/vector-types";
-import { constants, providers, utils, Wallet } from "ethers";
+import { constants, Contract, providers, Wallet } from "ethers";
 import { ChannelFactory, ChannelMastercopy, VectorOnchainService } from "@connext/vector-contracts";
 import { BaseLogger } from "pino";
 
@@ -27,12 +27,32 @@ export class VectorTransactionService extends VectorOnchainService implements IV
     });
   }
 
+  private async sendTxAndParseResponse(
+    txFn: Promise<providers.TransactionResponse>,
+  ): Promise<Result<providers.TransactionResponse, OnchainError>> {
+    try {
+      const tx = await txFn;
+      return Result.ok(tx);
+    } catch (e) {
+      let error = e;
+      if (e.message.includes("sender doesn't have enough funds")) {
+        error = new OnchainError(OnchainError.reasons.NotEnoughFunds);
+      }
+      return Result.fail(error);
+    }
+  }
+
   async sendDepositTx(
     channelState: FullChannelState<any>,
     sender: string,
     amount: string,
     assetId: string,
   ): Promise<Result<providers.TransactionResponse, OnchainError>> {
+    const signer = this.signers.get(channelState.networkContext.chainId);
+    if (!signer?._isSigner) {
+      return Result.fail(new OnchainError(OnchainError.reasons.SignerNotFound));
+    }
+
     if (!channelState.participants.includes(sender)) {
       return Result.fail(new OnchainError(OnchainError.reasons.SenderNotInChannel));
     }
@@ -47,22 +67,16 @@ export class VectorTransactionService extends VectorOnchainService implements IV
     if (multisigCode === `0x`) {
       this.logger.info({ method: "sendDepositTx", channelAddress: channelState.channelAddress }, `Deploying multisig`);
       // deploy multisig
-      const channelFactory = new utils.Interface(ChannelFactory.abi);
-      const data = channelFactory.encodeFunctionData("createChannel", [
-        channelState.participants[0],
-        channelState.participants[1],
-      ]);
-      console.log(
-        "channelState.networkContext.channelFactoryAddress: ",
+      const channelFactory = new Contract(
         channelState.networkContext.channelFactoryAddress,
+        ChannelFactory.abi,
+        signer,
       );
-      const txRes = await this.sendTx(
-        {
-          to: channelState.networkContext.channelFactoryAddress,
-          value: 0,
-          data,
-        },
-        channelState.networkContext.chainId,
+      const created = new Promise(res => {
+        channelFactory.once(channelFactory.filters.ChannelCreation(), res);
+      });
+      const txRes = await this.sendTxAndParseResponse(
+        channelFactory.createChannel(channelState.participants[0], channelState.participants[1]),
       );
       if (txRes.isError) {
         this.logger.error(
@@ -77,8 +91,18 @@ export class VectorTransactionService extends VectorOnchainService implements IV
       }
       const tx = txRes.getValue();
       this.logger.info({ method: "sendDepositTx", txHash: tx.hash }, "Deployed multisig, waiting for confirmation");
-      await tx.wait();
-      this.logger.info({ method: "sendDepositTx", txHash: tx.hash }, "Multisig deposit confirmed");
+      const txReceipt = await tx.wait();
+      const channelAddress = (await created) as string;
+      console.log("sendDepositTx:::::channelAddress: ", channelAddress);
+      this.logger.info(
+        {
+          method: "sendDepositTx",
+          txHash: txReceipt.transactionHash,
+          logs: txReceipt.logs,
+          events: (txReceipt as any).events,
+        },
+        "Multisig deposit confirmed",
+      );
     }
 
     if (sender === channelState.participants[0]) {
@@ -100,45 +124,37 @@ export class VectorTransactionService extends VectorOnchainService implements IV
     amount: string,
     assetId: string,
   ): Promise<Result<providers.TransactionResponse, OnchainError>> {
-    const vectorChannel = new utils.Interface(ChannelMastercopy.abi);
-    const data = vectorChannel.encodeFunctionData("depositA", [amount, assetId]);
-    if (assetId === constants.AddressZero) {
-      return this.sendTx(
-        {
-          data,
-          to: channelState.channelAddress,
-          value: amount,
-        },
-        channelState.networkContext.chainId,
-      );
-    } else {
+    const signer = this.signers.get(channelState.networkContext.chainId);
+    if (!signer?._isSigner) {
+      return Result.fail(new OnchainError(OnchainError.reasons.SignerNotFound));
+    }
+
+    if (assetId !== constants.AddressZero) {
       // need to approve
       this.logger.info({ assetId, channelAddress: channelState.channelAddress }, "Approving token");
-      const erc20 = new utils.Interface(ERC20Abi);
-      const data = erc20.encodeFunctionData("approve", [channelState.channelAddress, amount]);
-      const approveRes = await this.sendTx(
-        {
-          to: assetId,
-          value: 0,
-          data,
-        },
-        channelState.networkContext.chainId,
-      );
+      const erc20 = new Contract(channelState.networkContext.channelFactoryAddress, ERC20Abi, signer);
+      const approveRes = await this.sendTxAndParseResponse(erc20.approve(channelState.channelAddress, amount));
       if (approveRes.isError) {
+        this.logger.error(
+          {
+            method: "sendDepositATx",
+            channelAddress: channelState.channelAddress,
+            error: approveRes.getError()?.message,
+          },
+          "Error approving tokens for deposit A",
+        );
         return approveRes;
       }
       const approveTx = approveRes.getValue();
-      this.logger.info({ txHash: approveTx.hash }, "Approved token, waiting for confirmation");
-      await approveTx.wait();
-      return this.sendTx(
-        {
-          data,
-          to: channelState.channelAddress,
-          value: 0,
-        },
-        channelState.networkContext.chainId,
+      this.logger.info(
+        { txHash: approveTx.hash, method: "sendDepositATx", assetId },
+        "Approved token, waiting for confirmation",
       );
+      await approveTx.wait();
+      this.logger.info({ txHash: approveTx.hash, method: "sendDepositATx", assetId }, "Token approval confirmed");
     }
+    const vectorChannel = new Contract(channelState.channelAddress, ChannelMastercopy.abi, signer);
+    return this.sendTxAndParseResponse(vectorChannel.depositA(amount, assetId));
   }
 
   private async sendDepositBTx(
@@ -146,6 +162,11 @@ export class VectorTransactionService extends VectorOnchainService implements IV
     amount: string,
     assetId: string,
   ): Promise<Result<providers.TransactionResponse, OnchainError>> {
+    const signer = this.signers.get(channelState.networkContext.chainId);
+    if (!signer?._isSigner) {
+      return Result.fail(new OnchainError(OnchainError.reasons.SignerNotFound));
+    }
+
     if (assetId === constants.AddressZero) {
       return this.sendTx(
         {
@@ -156,16 +177,8 @@ export class VectorTransactionService extends VectorOnchainService implements IV
         channelState.networkContext.chainId,
       );
     } else {
-      const erc20 = new utils.Interface(ERC20Abi);
-      const data = erc20.encodeFunctionData("transfer", [channelState.channelAddress, amount]);
-      return this.sendTx(
-        {
-          to: assetId,
-          value: 0,
-          data,
-        },
-        channelState.networkContext.chainId,
-      );
+      const erc20 = new Contract(channelState.networkContext.channelFactoryAddress, ERC20Abi, signer);
+      return this.sendTxAndParseResponse(erc20.transfer(channelState.channelAddress, amount));
     }
   }
 
@@ -178,15 +191,6 @@ export class VectorTransactionService extends VectorOnchainService implements IV
       return Result.fail(new OnchainError(OnchainError.reasons.SignerNotFound));
     }
 
-    try {
-      const tx = await signer.sendTransaction(minTx);
-      return Result.ok(tx);
-    } catch (e) {
-      let error = e;
-      if (e.message.includes("sender doesn't have enough funds")) {
-        error = new OnchainError(OnchainError.reasons.NotEnoughFunds);
-      }
-      return Result.fail(error);
-    }
+    return this.sendTxAndParseResponse(signer.sendTransaction(minTx));
   }
 }
