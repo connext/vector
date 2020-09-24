21552eb81845d9226d582c92509bb74c0ba45030
LayneHaber <layne.haber@gmail.com>
2020-09-23 21:35:28 -0600

    resolve withdrawal commitment

--- modules/engine/src/listeners.ts
+++ modules/engine/src/listeners.ts
@@ -1,38 +1,94 @@
+import { WithdrawCommitment } from "@connext/vector-contracts";
 import {
   ChannelUpdateEvent,
   CreateUpdateDetails,
+  FullChannelState,
   IChannelSigner,
   IMessagingService,
   IVectorProtocol,
   ProtocolEventName,
   UpdateType,
+  WithdrawState,
 } from "@connext/vector-types";
+import { BigNumber } from "ethers";
+import Pino from "pino";
 
 export async function setupListeners(
   vector: IVectorProtocol,
   messaging: IMessagingService,
   signer: IChannelSigner,
+  logger: Pino.BaseLogger = Pino(),
 ): Promise<void> {
   // Set up withdraw listener and handler
   vector.on(
     ProtocolEventName.CHANNEL_UPDATE_EVENT,
-    data => handleWithdrawResolve(data),
-    data => {
+    event => handleWithdrawResolve(event, signer, vector, logger),
+    event => {
       const {
         updatedChannelState: {
           latestUpdate: { toIdentifier, type, details },
           networkContext: { withdrawDefinition },
         },
-      } = data;
+      } = event;
       return (
         toIdentifier === signer.publicIdentifier &&
         type === UpdateType.create &&
+        !!withdrawDefinition &&
         (details as CreateUpdateDetails).transferDefinition === withdrawDefinition
       );
     },
   );
 }
 
-async function handleWithdrawResolve(data: ChannelUpdateEvent): Promise<void> {
-  console.log("Reached here!");
+async function handleWithdrawResolve(
+  event: ChannelUpdateEvent,
+  signer: IChannelSigner,
+  vector: IVectorProtocol,
+  logger: Pino.BaseLogger = Pino(),
+): Promise<void> {
+  // If you receive a withdraw from your counterparty, you should
+  // resolve the withdrawal with your signature
+  const {
+    channelAddress,
+    participants,
+    latestUpdate: {
+      details: { transferId, transferInitialState },
+      assetId,
+    },
+  } = event.updatedChannelState as FullChannelState<typeof UpdateType.create>;
+
+  // Get the recipient + amount from the transfer state
+  const { balance, initiatorSignature, signers, data, nonce, fee } = transferInitialState as WithdrawState;
+
+  // TODO: properly account for fees?
+  const withdrawalAmount = balance.amount.reduce((prev, curr) => prev.add(curr), BigNumber.from(0));
+
+  // TODO: should inject validation to make sure that a withdrawal transfer
+  // is properly signed before its been merged into your channel
+  const commitment = new WithdrawCommitment(
+    channelAddress,
+    participants,
+    balance.to[0], // TODO: correct recipient
+    assetId,
+    withdrawalAmount.toString(),
+    nonce,
+  );
+
+  // Generate your signature on the withdrawal commitment
+  const responderSignature = await signer.signMessage(commitment.hashToSign());
+  // TODO: add sigs to commitment?
+
+  // Resolve the withdrawal
+  const resolveRes = await vector.resolve({ transferResolver: { responderSignature }, transferId, channelAddress });
+
+  // Handle the error
+  if (resolveRes.isError) {
+    logger.error(
+      { method: "handleWithdrawResolve", error: resolveRes.getError()!.message, transferId, channelAddress },
+      "Failed to resolve withdrawal",
+    );
+  }
+
+  // Withdrawal successfully resolved
+  logger.info({ channelAddress, amount: withdrawalAmount.toString(), assetId, transferId }, "Withdrawal resolved");
 }
--- modules/engine/src/testing/listeners.spec.ts
+++ modules/engine/src/testing/listeners.spec.ts
@@ -1,15 +1,28 @@
 import {
   ChannelUpdateEvent,
   IChannelSigner,
-  IVectorProtocol,
   ProtocolEventName,
   ProtocolEventPayloadsMap,
+  Result,
   UpdateType,
+  WithdrawResolverEncoding,
+  WithdrawState,
+  WithdrawStateEncoding,
 } from "@connext/vector-types";
-import { createTestChannelState, getRandomChannelSigner, mkAddress, mkPublicIdentifier } from "@connext/vector-utils";
+import {
+  createTestChannelState,
+  delay,
+  getRandomChannelSigner,
+  mkAddress,
+  mkPublicIdentifier,
+  mkSig,
+} from "@connext/vector-utils";
+import { Vector } from "@connext/vector-protocol";
 import { Evt } from "evt";
+import Sinon from "sinon";
 
 import { setupListeners } from "../listeners";
+import { expect } from "chai";
 
 // Adds a handler to an evt instance and returns the result
 // based on the input arguments
@@ -30,35 +43,76 @@ describe.only("listeners", () => {
   // vector instance
 
   const evt = Evt.create<ChannelUpdateEvent>();
-
-  const vector: IVectorProtocol = {
-    on: (
-      event: ProtocolEventName,
-      callback: (payload: ProtocolEventPayloadsMap[typeof event]) => void | Promise<void>,
-      filter: (payload) => boolean = _payload => true,
-    ) => evt.pipe(filter).attach(callback),
-  } as any;
-
+  const alice: IChannelSigner = getRandomChannelSigner();
+  const bob: IChannelSigner = getRandomChannelSigner();
   const messaging = {} as any;
 
-  const signer: IChannelSigner = getRandomChannelSigner();
+  // Set vector stub to interact with this EVT instance
+  const on = (
+    event: ProtocolEventName,
+    callback: (payload: ProtocolEventPayloadsMap[typeof event]) => void | Promise<void>,
+    filter: (payload) => boolean = _payload => true,
+  ) => evt.pipe(filter).attach(callback);
+
+  let vector: Sinon.SinonStubbedInstance<Vector>;
+
+  beforeEach(() => {
+    vector = Sinon.createStubInstance(Vector);
+    // TODO: good way to work with events and sinon?
+    vector.on = on as any;
+  });
+
+  afterEach(() => Sinon.restore());
 
   describe("withdraw", () => {
     it("should work", async () => {
-      await setupListeners(vector, messaging, signer);
+      await setupListeners(vector, messaging, bob);
+
+      const withdrawInitialState: WithdrawState = {
+        balance: { to: [alice.address, bob.address], amount: ["5", "0"] },
+        nonce: "1",
+        initiatorSignature: mkSig(),
+        signers: [alice.address, bob.address],
+        data: "0x",
+        fee: "1",
+      };
 
       const updatedChannelState = createTestChannelState(UpdateType.create, {
         latestUpdate: {
-          fromIdentifier: mkPublicIdentifier(),
-          details: { transferDefinition: mkAddress("0xdef") },
+          toIdentifier: bob.publicIdentifier,
+          details: {
+            transferDefinition: mkAddress("0xdef"),
+            transferInitialState: withdrawInitialState,
+            transferEncodings: [WithdrawStateEncoding, WithdrawResolverEncoding],
+          },
         },
         networkContext: {
           withdrawDefinition: mkAddress("0xdef"),
         },
       });
 
+      // Set the resolve mock to return a result
+      vector.resolve.resolves(
+        Result.ok(
+          createTestChannelState(UpdateType.resolve, {
+            latestUpdate: {
+              fromIdentifier: bob.publicIdentifier,
+            },
+          }),
+        ),
+      );
+
       // Post to the evt
       evt.post({ updatedChannelState });
+      // Wait a bit to give handler time to react
+      await delay(1500);
+
+      // Verify that resolve was called correctly
+      expect(vector.resolve.callCount).to.be.eq(1);
+      const { transferResolver, channelAddress, transferId } = vector.resolve.args[0][0];
+      expect(transferResolver).to.be.ok;
+      expect(channelAddress).to.be.eq(updatedChannelState.channelAddress);
+      expect(transferId).to.be.eq(updatedChannelState.latestUpdate.details.transferId);
     });
   });
 });
