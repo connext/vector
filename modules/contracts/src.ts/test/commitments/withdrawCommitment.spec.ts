ef77f6ea3ebdb05f44c08bb81696f9dab509d505
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-09-24 18:01:30 -0700

    Make tests pass

--- modules/contracts/src.ts/test/commitments/withdrawCommitment.spec.ts
+++ modules/contracts/src.ts/test/commitments/withdrawCommitment.spec.ts
@@ -5,82 +5,82 @@ import { WithdrawCommitment } from "../../commitments";
 import { expect, provider } from "../utils";
 
 describe("withdrawCommitment", () => {
-    let deployer: Wallet;
-    let channelMastercopy: Contract;
-    let token: Contract;
+  let deployer: Wallet;
+  let channelMastercopy: Contract;
+  let token: Contract;
 
-    const participantA = getRandomChannelSigner(provider);
-    const participantB = getRandomChannelSigner(provider);
-    const amount = "50"
+  const participantA = getRandomChannelSigner(provider);
+  const participantB = getRandomChannelSigner(provider);
+  const amount = "50";
 
-    beforeEach(async () => {
-        deployer = provider.getWallets()[0];
-        channelMastercopy = await new ContractFactory(ChannelMastercopy.abi, ChannelMastercopy.bytecode, deployer).deploy();
-        await channelMastercopy.deployed();
-        token = await new ContractFactory(TestToken.abi, TestToken.bytecode, deployer).deploy("Test", "TST");
-        await channelMastercopy.deployed();
-        await channelMastercopy.setup([participantA.address, participantB.address])
+  beforeEach(async () => {
+    deployer = provider.getWallets()[0];
+    channelMastercopy = await new ContractFactory(ChannelMastercopy.abi, ChannelMastercopy.bytecode, deployer).deploy();
+    await channelMastercopy.deployed();
+    token = await new ContractFactory(TestToken.abi, TestToken.bytecode, deployer).deploy("Test", "TST");
+    await channelMastercopy.deployed();
+    await channelMastercopy.setup([participantA.address, participantB.address]);
 
-        // Fund with Eth and tokens
-        await deployer.sendTransaction({to: channelMastercopy.address, value: BigNumber.from(amount).mul(2)})
-        await token.mint(channelMastercopy.address, BigNumber.from(amount).mul(2));
-    })
-        
-    it("can create the commitment", () => {
-        const commitment = new WithdrawCommitment(
-            channelMastercopy.address,
-            [participantA.address, participantB.address],
-            participantA.address,
-            constants.AddressZero,
-            amount,
-            "1"
-        )
-        expect(commitment).to.be.ok
-    })
+    // Fund with Eth and tokens
+    await deployer.sendTransaction({ to: channelMastercopy.address, value: BigNumber.from(amount).mul(2) });
+    await token.mint(channelMastercopy.address, BigNumber.from(amount).mul(2));
+  });
 
-    it("can successfully withdraw Eth", async () => {
-        const commitment = new WithdrawCommitment(
-            channelMastercopy.address,
-            [participantA.address, participantB.address],
-            participantA.address,
-            constants.AddressZero,
-            amount,
-            "1"
-        )
-        const signatureA = await participantA.signMessage(commitment.hashToSign())
-        const signatureB = await participantB.signMessage(commitment.hashToSign())
-        await commitment.addSignatures(signatureA, signatureB);
+  it("can create the commitment", () => {
+    const commitment = new WithdrawCommitment(
+      channelMastercopy.address,
+      [participantA.address, participantB.address],
+      participantA.address,
+      constants.AddressZero,
+      amount,
+      "1",
+    );
+    expect(commitment).to.be.ok;
+  });
 
-        const tx = await commitment.getSignedTransaction();
+  it("can successfully withdraw Eth", async () => {
+    const commitment = new WithdrawCommitment(
+      channelMastercopy.address,
+      [participantA.address, participantB.address],
+      participantA.address,
+      constants.AddressZero,
+      amount,
+      "1",
+    );
+    const signatureA = await participantA.signMessage(commitment.hashToSign());
+    const signatureB = await participantB.signMessage(commitment.hashToSign());
+    await commitment.addSignatures(signatureA, signatureB);
 
-        // Check before balance
-        expect((await channelMastercopy.getBalance(constants.AddressZero)).eq(BigNumber.from(amount).mul(2)))
-        await deployer.sendTransaction(tx)
+    const tx = await commitment.getSignedTransaction();
 
-        // Check after balance
-        expect((await channelMastercopy.getBalance(constants.AddressZero)).eq(BigNumber.from(amount)))
-    })
+    // Check before balance
+    expect((await channelMastercopy.getBalance(constants.AddressZero)).eq(BigNumber.from(amount).mul(2)));
+    await deployer.sendTransaction(tx);
 
-    it("can successfully withdraw Tokens", async () => {
-        const commitment = new WithdrawCommitment(
-            channelMastercopy.address,
-            [participantA.address, participantB.address],
-            participantA.address,
-            token.address,
-            amount,
-            "1"
-        )
-        const signatureA = await participantA.signMessage(commitment.hashToSign())
-        const signatureB = await participantB.signMessage(commitment.hashToSign())
-        await commitment.addSignatures(signatureA, signatureB);
+    // Check after balance
+    expect((await channelMastercopy.getBalance(constants.AddressZero)).eq(BigNumber.from(amount)));
+  });
 
-        const tx = await commitment.getSignedTransaction();
+  it("can successfully withdraw Tokens", async () => {
+    const commitment = new WithdrawCommitment(
+      channelMastercopy.address,
+      [participantA.address, participantB.address],
+      participantA.address,
+      token.address,
+      amount,
+      "1",
+    );
+    const signatureA = await participantA.signMessage(commitment.hashToSign());
+    const signatureB = await participantB.signMessage(commitment.hashToSign());
+    await commitment.addSignatures(signatureA, signatureB);
 
-        // Check before balance
-        expect((await channelMastercopy.getBalance(token.address)).eq(BigNumber.from(amount).mul(2)))
-        await deployer.sendTransaction(tx)
+    const tx = await commitment.getSignedTransaction();
 
-        // Check after balance
-        expect((await channelMastercopy.getBalance(token.address)).eq(BigNumber.from(amount)))
-    })
-})
+    // Check before balance
+    expect((await channelMastercopy.getBalance(token.address)).eq(BigNumber.from(amount).mul(2)));
+    await deployer.sendTransaction(tx);
+
+    // Check after balance
+    expect((await channelMastercopy.getBalance(token.address)).eq(BigNumber.from(amount)));
+  });
+});
