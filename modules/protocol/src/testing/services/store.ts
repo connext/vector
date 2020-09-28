b707d3a90b69d70b9b7bbb1262413d86e5f1add9
LayneHaber <layne.haber@gmail.com>
2020-09-28 10:30:35 -0600

    prettier

--- modules/protocol/src/testing/services/store.ts
+++ modules/protocol/src/testing/services/store.ts
@@ -38,7 +38,7 @@ export class MemoryStoreService implements IVectorStore {
     chainId: number,
   ): Promise<FullChannelState<any> | undefined> {
     return Promise.resolve(
-      [...this.channelStates.values()].find((channelState) => {
+      [...this.channelStates.values()].find(channelState => {
         channelState.state.participants[0] === participantA &&
           channelState.state.participants[1] === participantB &&
           channelState.state.networkContext.chainId === chainId;
@@ -47,7 +47,7 @@ export class MemoryStoreService implements IVectorStore {
   }
 
   getChannelStates(): Promise<FullChannelState[]> {
-    return Promise.resolve([...this.channelStates.values()].map((c) => c.state));
+    return Promise.resolve([...this.channelStates.values()].map(c => c.state));
   }
 
   saveChannelState(
@@ -79,7 +79,7 @@ export class MemoryStoreService implements IVectorStore {
       // This is a `resolve` update, so remove from channel
       this.transfersInChannel.set(
         channelState.channelAddress,
-        activeTransfers.filter((x) => x !== transfer.transferId),
+        activeTransfers.filter(x => x !== transfer.transferId),
       );
       return Promise.resolve();
     }
@@ -91,7 +91,7 @@ export class MemoryStoreService implements IVectorStore {
 
   getActiveTransfers(channelAddress: string): Promise<FullTransferState[]> {
     const active = [...(this.transfersInChannel.get(channelAddress) ?? [])];
-    const all = active.map((id) => this.transfers.get(id)).filter((x) => !!x);
+    const all = active.map(id => this.transfers.get(id)).filter(x => !!x);
     return Promise.resolve(all as FullTransferState[]);
   }
 
