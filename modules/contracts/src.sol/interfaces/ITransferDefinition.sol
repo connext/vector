b575010d7c2c49b558045355702ef4c3c33badd2
LayneHaber <layne.haber@gmail.com>
2020-10-06 09:02:41 -0600

    prettier

--- modules/contracts/src.sol/interfaces/ITransferDefinition.sol
+++ modules/contracts/src.sol/interfaces/ITransferDefinition.sol
@@ -4,21 +4,12 @@ pragma experimental "ABIEncoderV2";
 
 import "./Types.sol";
 
-
 interface ITransferDefinition {
+  // Validates the initial state of the transfer.
+  // Called by validator.ts during `create` updates.
+  function create(bytes calldata) external view returns (bool);
 
-    // Validates the initial state of the transfer.
-    // Called by validator.ts during `create` updates.
-    function create(bytes calldata)
-        external
-        view
-        returns (bool);
-
-    // Performs a state transition to resolve a transfer and returns final balances.
-    // Called by validator.ts during `resolve` updates.
-    function resolve(bytes calldata, bytes calldata)
-        external
-        view
-        returns (Balance memory);
-
+  // Performs a state transition to resolve a transfer and returns final balances.
+  // Called by validator.ts during `resolve` updates.
+  function resolve(bytes calldata, bytes calldata) external view returns (Balance memory);
 }
