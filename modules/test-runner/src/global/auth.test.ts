dbec99930f5ad933020dcc96bebb825356de4738
bohendo <bohende@gmail.com>
2020-10-08 23:39:57 +0530

    🚿 cleanup tests

--- modules/test-runner/src/global/auth.test.ts
+++ modules/test-runner/src/global/auth.test.ts
@@ -1,5 +1,4 @@
 import {
-  delay,
   NatsMessagingService,
   expect,
   getBearerTokenFunction,
@@ -22,17 +21,19 @@ describe("Global Auth Service", () => {
   });
 
   it("should dispense a jwt which allows the user to connect to NATS", async () => {
+    const testSubject = `${recipientId}.${senderId}.test.subject`;
     const opts = { messagingUrl: env.natsUrl };
+
     const nonceRes = await axios.get(`${env.authUrl}/auth/${recipientId}`);
     expect(nonceRes.status).to.equal(200);
     expect(nonceRes.data).to.be.a("string");
+
     const sig = await recipient.signMessage(nonceRes.data);
     const verifyRes = await axios.post(`${env.authUrl}/auth`, { sig, userIdentifier: recipientId });
     expect(verifyRes.status).to.equal(200);
     expect(verifyRes.data).to.be.a("string");
 
     const recipientMessaging = new NatsMessagingService(opts, pino(), () => verifyRes.data);
-    expect(recipientMessaging).to.be.ok;
     await recipientMessaging.connect();
 
     const received = new Promise((res, rej) => {
@@ -45,18 +46,10 @@ describe("Global Auth Service", () => {
       pino(),
       getBearerTokenFunction(sender, env.authUrl),
     );
-    expect(senderMessaging).to.be.ok;
     await senderMessaging.connect();
 
-    const testSubject = `${senderId}.${recipientId}.test.subject`;
-
-    console.log(`Waiting for message`);
-    await delay(3000);
-
-    console.log(`Sending message`);
-    await expect(recipientMessaging.publish(testSubject, { hello: "world" })).to.be.fulfilled;
+    await expect(senderMessaging.publish(testSubject, { hello: "world" })).to.be.fulfilled;
     await expect(received).to.be.fulfilled;
-
   });
 
 });
