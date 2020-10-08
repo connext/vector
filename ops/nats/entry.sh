b5c3dd271bfb5ba8c130ac976162f71f26c99542
bohendo <bohende@gmail.com>
2020-10-08 21:58:11 +0530

    🐛 fix misnamed env var

--- ops/nats/entry.sh
+++ ops/nats/entry.sh
@@ -4,7 +4,7 @@ set -e
 if [[ -n "$JWT_SIGNER_PUBLIC_KEY" ]]
 then echo "Using public key provided by env var"
 elif [[ -n "$JWT_SIGNER_PUBLIC_KEY_FILE" ]]
-then export JWT_SIGNER_PUBLIC_KEY_FILE="`cat $JWT_SIGNER_PUBLIC_KEY_FILE`"
+then export JWT_SIGNER_PUBLIC_KEY="`cat $JWT_SIGNER_PUBLIC_KEY_FILE`"
 else echo "public key must be provided via either a secret or an env var." && exit 1
 fi
 
