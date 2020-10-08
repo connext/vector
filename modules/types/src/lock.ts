d25f459e096454c4a34bd764a2135abec89431ad
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-10-07 17:05:42 -0700

    Lockservice interface changes

--- modules/types/src/lock.ts
+++ modules/types/src/lock.ts
@@ -1,10 +1,14 @@
 export interface ILockService {
   acquireLock(
     lockName: string /* Bytes32? */,
+    isAlice?: boolean,
+    counterpartyPublicIdentifier?: string,
   ): Promise<string>;
 
   releaseLock(
     lockName: string /* Bytes32? */,
     lockValue: string,
+    isAlice?: boolean,
+    counterpartyPublicIdentifier?: string,
   ): Promise<void>;
 }
