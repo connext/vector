1beeb9079b969beac445b15e0f42c67e0ad48ffb
arjunbhuptani@gmail.com <arjunbhuptani@gmail.com>
2020-10-09 14:41:47 -0700

    Flesh out load tests

--- modules/test-runner/src/load/test.ts
+++ modules/test-runner/src/load/test.ts
@@ -1,16 +1,17 @@
-import { EngineEvents, ServerNodeResponses } from "@connext/vector-types";
-import { RestServerNodeService } from "@connext/vector-utils";
+import { EngineEvents, ServerNodeResponses, TransferNames } from "@connext/vector-types";
+import { getRandomBytes32, RestServerNodeService } from "@connext/vector-utils";
 import { constants, providers, utils, Wallet } from "ethers";
 
 import { env, getRandomIndex } from "../utils";
 
 import { config } from "./config";
 
-import { logger } from "./index";
+import { carolEvts, logger } from "./index";
 
 const chainId = parseInt(Object.keys(env.chainProviders)[0]);
 const wallet = Wallet.fromMnemonic(env.sugarDaddy).connect(new providers.JsonRpcProvider(env.chainProviders[chainId]));
 const assetId = constants.AddressZero;
+const transferAmount = utils.parseEther("0.0001").toString();
 
 type Nodes = {
   publicIdentifier: string;
@@ -20,6 +21,10 @@ type Nodes = {
 
 const preImages: {
   [routingId: string]: string;
+} = {};
+
+const channelAddresses: {
+  [publicIdentifier: string]: string;
 } = {};
 
 const createNodes = async (
@@ -42,35 +47,50 @@ const createNodes = async (
   return nodes;
 };
 
-const prepNodes = async (
-  nodes: ServerNodeResponses.GetConfig,
-  node: RestServerNodeService,
-  counterpartyNodes: ServerNodeResponses.GetConfig,
-) => {
+const prepNodes = async (nodes: ServerNodeResponses.GetConfig, node: RestServerNodeService) => {
   const getRandomCounterparty = () => {
-    const random = Math.floor(Math.random() * counterpartyNodes.length);
-    return counterpartyNodes[random];
+    const filteredNodes = nodes.filter(n => {
+      return n.publicIdentifier !== node.publicIdentifier;
+    });
+    const random = Math.floor(Math.random() * filteredNodes.length);
+    return filteredNodes[random];
   };
   for (const n of nodes) {
     logger.info({ node: n }, "Setting up node");
-    const setup = await node.requestSetup({
-      aliceUrl: env.rogerUrl,
+    const roger = await RestServerNodeService.connect(env.rogerUrl, logger.child({ module: "RestServerNodeService" }));
+    const channel = await node.getStateChannelByParticipants({
+      alice: roger.publicIdentifier,
+      bob: n.publicIdentifier,
       chainId,
-      timeout: "360000",
-      bobIdentifier: n.publicIdentifier,
     });
-    if (setup.isError) {
-      logger.error({ error: setup.getError() }, "Could not set up");
-      process.exit(1);
-    }
-    logger.info({ res: setup.getValue() }, "Setup node");
 
-    const tx = await wallet.sendTransaction({ to: setup.getValue().channelAddress, value: utils.parseEther("0.5") });
+    let channelAddress;
+
+    if (channel.isError) {
+      const setup = await node.requestSetup({
+        aliceUrl: env.rogerUrl,
+        chainId,
+        timeout: "360000",
+        bobIdentifier: n.publicIdentifier,
+      });
+      if (setup.isError) {
+        logger.error({ error: setup.getError() }, "Could not set up");
+        process.exit(1);
+      }
+      logger.info({ res: setup.getValue() }, "Setup node");
+      channelAddress = setup.getValue().channelAddress;
+    } else {
+      // Channel already exists, don't need to setup
+      channelAddress = channel.getValue().channelAddress;
+    }
+
+    channelAddresses[node.publicIdentifier] = channelAddress;
+    const tx = await wallet.sendTransaction({ to: channelAddress, value: utils.parseEther("0.5") });
     await tx.wait();
 
     const deposit = await node.reconcileDeposit({
       assetId,
-      channelAddress: setup.getValue().channelAddress,
+      channelAddress: channelAddress,
       publicIdentifier: n.publicIdentifier,
     });
     if (deposit.isError) {
@@ -99,27 +119,88 @@ const prepNodes = async (
           process.exit(1);
         }
         logger.info({ res: resolve.getValue() }, "Resolved transfer");
+      },
+      data => data.channelAddress === channelAddress && data.transfer.responder === node.signerAddress,
+    );
 
+    node.on(
+      EngineEvents.CONDITIONAL_TRANSFER_RESOLVED,
+      async data => {
+        const preImage = getRandomBytes32();
+        const lockHash = utils.soliditySha256(["bytes32"], [preImage]);
+        const routingId = getRandomBytes32();
+        preImages[routingId] = preImage;
         const counterparty = getRandomCounterparty();
 
-        logger.info({ counterparty }, "Resolved transfer");
+        logger.info({ counterparty }, "Creating transfer");
+
+        const create = await node.conditionalTransfer({
+          channelAddress: data.channelAddress,
+          amount: transferAmount,
+          assetId,
+          recipient: counterparty.publicIdentifier,
+          type: TransferNames.HashlockTransfer,
+          meta: {
+            routingId,
+          },
+          details: {
+            lockHash,
+            expiry: "0",
+          },
+        });
+
+        if (create.isError) {
+          logger.error({ error: create.getError() }, "Could not create transfer");
+          process.exit(1);
+        }
+        logger.info({ res: create.getValue() }, "Created transfer");
       },
-      data => data.channelAddress === setup.getValue().channelAddress,
+      data => data.channelAddress === channelAddress && data.transfer.responder === node.signerAddress,
     );
   }
 };
 
 export const test = async (): Promise<void> => {
-  const carol = await RestServerNodeService.connect(env.carolUrl, logger.child({ module: "RestServerNodeService" }));
+  const carol = await RestServerNodeService.connect(
+    env.carolUrl,
+    logger.child({ module: "RestServerNodeService" }),
+    carolEvts,
+  );
   const carolNodesRes = await carol.getConfig();
   let carolNodes = carolNodesRes.getValue();
   carolNodes = await createNodes(carolNodes, carol);
+  await prepNodes(carolNodes, carol);
 
-  const dave = await RestServerNodeService.connect(env.daveUrl, logger.child({ module: "RestServerNodeService" }));
-  const daveNodesRes = await dave.getConfig();
-  let daveNodes = daveNodesRes.getValue();
-  daveNodes = await createNodes(daveNodes, dave);
+  const sender = carolNodes.pop();
 
   for (const node of carolNodes) {
+    const createPreImage = getRandomBytes32();
+    const lockHash = utils.soliditySha256(["bytes32"], [createPreImage]);
+    const routingId = getRandomBytes32();
+    preImages[routingId] = createPreImage;
+    const channelAddress = channelAddresses[node.publicIdentifier];
+
+    logger.info("=================== Creating conditionl transfer");
+    const create = await carol.conditionalTransfer({
+      channelAddress,
+      amount: transferAmount,
+      assetId,
+      recipient: node.publicIdentifier,
+      type: TransferNames.HashlockTransfer,
+      meta: {
+        routingId,
+      },
+      details: {
+        lockHash,
+        expiry: "0",
+      },
+    });
+    logger.info("======================= Created conditional transfer");
+
+    if (create.isError) {
+      logger.error({ error: create.getError() }, "Could not create transfer");
+      process.exit(1);
+    }
+    logger.info({ res: create.getValue() }, "Created transfer");
   }
 };
