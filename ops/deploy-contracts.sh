6a9064a2156f65331655c0e3338b9d721efd5d8f
bohendo <bohende@gmail.com>
2020-10-13 13:30:44 +0530

    ✨ ops/deploy-contracts

---
+++ ops/deploy-contracts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+root="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+project="`cat $root/package.json | grep '"name":' | head -n 1 | cut -d '"' -f 4`"
+
+docker run \
+  --entrypoint=node \
+  --interactive \
+  --name="${project}_contract_deployer" \
+  --rm \
+  --tmpfs="/tmp" \
+  --tty \
+  --volume="$root/address-book.json:/data/address-book.json" \
+  ${project}_ethprovider:latest dist/cli.js migrate --address-book=/data/address-book.json "$@"
