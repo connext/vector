70bcb874999310baf2482e42b0193e0fddd7e0d6
bohendo <bohende@gmail.com>
2020-10-13 13:58:05 +0530

    📔 tweak address-book format

--- ops/deploy-contracts.sh
+++ ops/deploy-contracts.sh
@@ -4,12 +4,15 @@ set -e
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 project="`cat $root/package.json | grep '"name":' | head -n 1 | cut -d '"' -f 4`"
 
+args="${@:---help}"
+
 docker run \
   --entrypoint=node \
   --interactive \
   --name="${project}_contract_deployer" \
+  --network="${project}" \
   --rm \
   --tmpfs="/tmp" \
   --tty \
   --volume="$root/address-book.json:/data/address-book.json" \
-  ${project}_ethprovider:latest dist/cli.js migrate --address-book=/data/address-book.json "$@"
+  ${project}_ethprovider:latest dist/cli.js migrate --address-book=/data/address-book.json "$args"
