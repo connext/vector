e36aa8fce1f977a4b5d99fc5d6f5598a0e263908
LayneHaber <layne.haber@gmail.com>
2020-10-15 00:52:59 -0400

    add test

--- modules/test-runner/src/duet/config.test.ts
+++ modules/test-runner/src/duet/config.test.ts
@@ -1,13 +1,32 @@
-import { expect } from "@connext/vector-utils";
+import { expect, RestServerNodeService, mkAddress } from "@connext/vector-utils";
 import axios from "axios";
 
-import { env } from "../utils";
+import { env, getTestLoggers } from "../utils";
 
-describe("Duet Config", () => {
+const testName = "Duet Config";
+
+const { log } = getTestLoggers(testName);
+
+describe(testName, () => {
   it("alice & bob should pong when pinged", async () => {
     const alicePong = (await axios.get(`${env.aliceUrl}/ping`)).data;
     const bobPong = (await axios.get(`${env.bobUrl}/ping`)).data;
     expect(alicePong).to.equal("pong\n");
     expect(alicePong).to.equal(bobPong);
+  });
+
+  it("should work with a default identifier", async () => {
+    const alice = await RestServerNodeService.connect(env.aliceUrl, log, undefined, 0);
+
+    const res = await alice.getConfig();
+    const val = res.getValue();
+    expect(val.length).to.be.deep.eq(1);
+    expect(val[0].signerAddress).to.be.ok;
+    expect(val[0].publicIdentifier).to.be.ok;
+
+    const test = await alice.getStateChannel({ channelAddress: mkAddress("0xccc") });
+
+    const err = test.getError()!;
+    expect(err.context.stack).to.be.ok;
   });
 });
