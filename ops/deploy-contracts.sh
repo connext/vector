5c9fbb80619783cb33921a2a0e14f6460abcad6f
bohendo <bohende@gmail.com>
2020-10-20 11:36:21 +0530

    🔧 [deploy-contracts] create a docker net if one doesn't exist

--- ops/deploy-contracts.sh
+++ ops/deploy-contracts.sh
@@ -4,6 +4,10 @@ set -e
 root=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )
 project=$(grep -m 1 '"name":' "$root/package.json" | cut -d '"' -f 4)
 
+# make sure a network for this project has been created
+docker swarm init 2> /dev/null || true
+docker network create --attachable --driver overlay "$project" 2> /dev/null || true
+
 args="${*:---help}"
 
 ethprovider_image="${project}_ethprovider:latest";
