6f3518741c5539ff2dcfd4dd0da78ef5da2906e7
LayneHaber <layne.haber@gmail.com>
2020-10-21 00:44:29 -0400

    WIP AssetTransfer tests

--- modules/contracts/src.ts/tests/assetTransfer.spec.ts
+++ modules/contracts/src.ts/tests/assetTransfer.spec.ts
@@ -1,8 +1,61 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 
-describe("AssetTransfer.sol", () => {
-  it.skip("should successfully transfer eth", async () => {});
-  it.skip("should successfully transfer erc20", async () => {});
-  it.skip("should successfully transfer assets if `transfer` does not return boolean", async () => {});
-  it.skip("should make funds withdrawable if transfer fails", async () => {});
+import { AddressZero } from "@ethersproject/constants";
+import { expect } from "chai";
+import { BigNumber, Contract } from "ethers";
+
+import { deployContracts } from "..";
+import { AddressBook } from "../addressBook";
+
+import { alice, bob } from "./constants";
+import { getTestAddressBook, getTestChannel } from "./utils";
+
+// TODO: best way to test this?
+describe.only("AssetTransfer.sol", () => {
+  let addressBook: AddressBook;
+  let assetTransfer: Contract;
+  let channel: Contract;
+
+  before(async () => {
+    addressBook = await getTestAddressBook();
+    await deployContracts(bob, addressBook, [["AssetTransfer", []]]);
+    assetTransfer = addressBook.getContract("AssetTransfer");
+    // NOTE: safe to do because of inheritance pattern
+    channel = await getTestChannel(addressBook);
+  });
+
+  it("should deploy", async () => {
+    expect(assetTransfer.address).to.be.a("string");
+  });
+
+  // TODO: how to change total transferred from test?
+  describe("getTotalTransferred", () => {
+    it("should fail if called directly", async () => {
+      await expect(assetTransfer.getTotalTransferred(AddressZero)).revertedWith("This contract is the mastercopy");
+    });
+
+    // TODO: should you be able to call this on the channel?
+    it.skip("should work", async () => {
+      expect(await channel.getTotalTransferred(AddressZero)).to.be.eq(BigNumber.from(0));
+    });
+  });
+
+  // TODO: how to change withdrawal amount from test?
+  describe("getEmergencyWithdrawableAmount", () => {
+    it("should fail if called directly", async () => {
+      await expect(assetTransfer.getEmergencyWithdrawableAmount(AddressZero, bob.address)).revertedWith(
+        "This contract is the mastercopy",
+      );
+    });
+
+    // TODO: should you be able to call this on the channel?
+    it.skip("should work", async () => {
+      expect(await channel.getEmergencyWithdrawableAmount(AddressZero, bob.address)).to.be.eq(BigNumber.from(0));
+    });
+  });
+
+  // TODO: how to change withdrawal amount from test?
+  describe.skip("emergencyWithdraw", () => {
+    it("should work", async () => {});
+  });
 });
