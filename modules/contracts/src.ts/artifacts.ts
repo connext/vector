13f2684075da4c2275832cf24f58b8f78f3d2738
LayneHaber <layne.haber@gmail.com>
2020-10-22 01:02:04 -0400

    Fix test utilities

--- modules/contracts/src.ts/artifacts.ts
+++ modules/contracts/src.ts/artifacts.ts
@@ -4,6 +4,7 @@ import { utils } from "ethers";
 import * as AssetTransfer from "../artifacts/AssetTransfer.json";
 import * as ChannelFactory from "../artifacts/ChannelFactory.json";
 import * as ChannelMastercopy from "../artifacts/ChannelMastercopy.json";
+import * as FailingToken from "../artifacts/FailingToken.json";
 import * as HashlockTransfer from "../artifacts/HashlockTransfer.json";
 import * as Proxy from "../artifacts/Proxy.json";
 import * as TestToken from "../artifacts/TestToken.json";
@@ -11,6 +12,7 @@ import * as TransferDefinition from "../artifacts/ITransferDefinition.json";
 import * as TransferRegistry from "../artifacts/TransferRegistry.json";
 import * as VectorChannel from "../artifacts/IVectorChannel.json";
 import * as Withdraw from "../artifacts/Withdraw.json";
+import * as TestChannel from "../artifacts/TestChannel.json";
 
 type Abi = Array<string | utils.FunctionFragment | utils.EventFragment | utils.ParamType>;
 
@@ -27,24 +29,28 @@ export const artifacts: Artifacts = {
   AssetTransfer,
   ChannelFactory,
   ChannelMastercopy,
+  FailingToken,
   HashlockTransfer,
   Proxy,
+  TestChannel,
   TestToken,
   TransferDefinition,
+  TransferRegistry,
   VectorChannel,
   Withdraw,
-  TransferRegistry,
 } as any;
 
 export {
   AssetTransfer,
   ChannelFactory,
   ChannelMastercopy,
+  FailingToken,
   HashlockTransfer,
   Proxy,
+  TestChannel,
   TestToken,
   TransferDefinition,
+  TransferRegistry,
   VectorChannel,
   Withdraw,
-  TransferRegistry,
 };
