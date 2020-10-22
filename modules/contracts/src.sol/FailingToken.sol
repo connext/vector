f655cb147ef08cf0b3dd5ee823886a5beca85e32
LayneHaber <layne.haber@gmail.com>
2020-10-22 01:01:23 -0400

    Test fixtures

---
+++ modules/contracts/src.sol/FailingToken.sol
@@ -0,0 +1,33 @@
+    
+    
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.1;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+/* This token is ONLY useful for testing
+ * Anybody can mint as many tokens as they like
+ * Anybody can burn anyone else's tokens
+ * Will fail to transfer ANY tokens
+ */
+contract FailingToken is ERC20 {
+
+    constructor () ERC20("Failing Token", "FAIL") {
+      _mint(msg.sender, 1000000 ether);
+    }
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+      require(false, "Failing token never transfers");
+    }
+
+}
---
+++ modules/contracts/src.sol/TestChannel.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.1;
+pragma experimental "ABIEncoderV2";
+
+import "./ChannelMastercopy.sol";
+import "./interfaces/ITestChannel.sol";
+
+/// @title TestChannel
+/// @author Layne Haber <layne@connext.network>
+/// @notice This contract will help test the `ChannelMastercopy` contract and
+///         the associated bits of functionality. This contract should *only*
+///         contain aliases to internal functions that should be unit-tested,
+///         like the `transferAsset` call on `AssetTransfer.sol`. Using this
+///         contract will help reduce the amount of boilerplate needed to test
+///         component functionality. For example, `AssetTransfer.sol` is only
+///         able to be tested via the adjudicator in many practical cases.
+///         Creating a helper function allows for easier testing of only
+///         that functionality.
+
+contract TestChannel is ChannelMastercopy, ITestChannel {
+  function assetTransfer(
+    address assetId,
+    address payable recipient,
+    uint256 maxAmount
+  ) public override returns (bool) {
+    return transferAsset(assetId, recipient, maxAmount);
+  }
+}
