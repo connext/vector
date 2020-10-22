24da0e2add8c9d0df244f95ba5e48426a3181b46
LayneHaber <layne.haber@gmail.com>
2020-10-22 01:01:12 -0400

    Linter :(

--- modules/contracts/src.sol/AssetTransfer.sol
+++ modules/contracts/src.sol/AssetTransfer.sol
@@ -9,148 +9,133 @@ import "./lib/LibUtils.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
 contract AssetTransfer is CMCCore, IAssetTransfer {
+  using SafeMath for uint256;
 
-    using SafeMath for uint256;
+  // TODO: These are ad hoc values. Confirm or find more suitable ones.
+  uint256 private constant ETHER_TRANSFER_GAS_LIMIT = 10000;
+  uint256 private constant ERC20_TRANSFER_GAS_LIMIT = 100000;
+  uint256 private constant ERC20_BALANCE_GAS_LIMIT = 5000;
 
-    // TODO: These are ad hoc values. Confirm or find more suitable ones.
-    uint256 private constant ETHER_TRANSFER_GAS_LIMIT = 10000;
-    uint256 private constant ERC20_TRANSFER_GAS_LIMIT = 100000;
-    uint256 private constant ERC20_BALANCE_GAS_LIMIT = 5000;
+  mapping(address => uint256) internal totalTransferred;
+  mapping(address => mapping(address => uint256)) private emergencyWithdrawableAmount;
 
-    mapping(address => uint256) internal totalTransferred;
-    mapping(address => mapping(address => uint256)) private emergencyWithdrawableAmount;
+  modifier onlySelf() {
+    require(msg.sender == address(this), "AssetTransfer: Can only be called from this contract");
+    _;
+  }
 
-    modifier onlySelf() {
-        require(
-            msg.sender == address(this),
-            "AssetTransfer: Can only be called from this contract"
-        );
-        _;
+  function safelyTransferEther(address payable recipient, uint256 maxAmount) private returns (bool, uint256) {
+    uint256 balance = address(this).balance;
+    uint256 amount = LibUtils.min(maxAmount, balance);
+    (bool success, ) = recipient.call{gas: ETHER_TRANSFER_GAS_LIMIT, value: amount}("");
+    return (success, success ? amount : 0);
+  }
+
+  function safelyTransferERC20(
+    address assetId,
+    address recipient,
+    uint256 maxAmount
+  ) private returns (bool, uint256) {
+    (bool success, bytes memory encodedReturnValue) = address(this).call{gas: ERC20_BALANCE_GAS_LIMIT}(
+      abi.encodeWithSignature("_getOwnERC20Balance(address)", assetId)
+    );
+    if (!success) {
+      return (false, 0);
     }
 
-    function safelyTransferEther(address payable recipient, uint256 maxAmount)
-        private
-        returns (bool, uint256)
-    {
-        uint256 balance = address(this).balance;
-        uint256 amount = LibUtils.min(maxAmount, balance);
-        (bool success, ) = recipient.call{gas: ETHER_TRANSFER_GAS_LIMIT, value: amount}("");
-        return (success, success ? amount : 0);
+    uint256 balance = abi.decode(encodedReturnValue, (uint256));
+    uint256 amount = LibUtils.min(maxAmount, balance);
+    (success, ) = address(this).call{gas: ERC20_TRANSFER_GAS_LIMIT}(
+      abi.encodeWithSignature("_transferERC20(address,address,uint256)", assetId, recipient, amount)
+    );
+    if (!success) {
+      return (false, 0);
+    }
+    return (success, success ? amount : 0);
+  }
+
+  function safelyTransfer(
+    address assetId,
+    address payable recipient,
+    uint256 maxAmount
+  ) private returns (bool, uint256) {
+    return
+      LibAsset.isEther(assetId)
+        ? safelyTransferEther(recipient, maxAmount)
+        : safelyTransferERC20(assetId, recipient, maxAmount);
+  }
+
+  function _getOwnERC20Balance(address assetId) external view onlySelf returns (uint256) {
+    return IERC20(assetId).balanceOf(address(this));
+  }
+
+  function _transferERC20(
+    address assetId,
+    address recipient,
+    uint256 amount
+  ) external onlySelf returns (bool) {
+    return LibAsset.transferERC20(assetId, recipient, amount);
+  }
+
+  function registerTransfer(address assetId, uint256 amount) internal {
+    totalTransferred[assetId] += amount;
+  }
+
+  function addToEmergencyWithdrawableAmount(
+    address assetId,
+    address owner,
+    uint256 amount
+  ) internal {
+    emergencyWithdrawableAmount[assetId][owner] += amount;
+  }
+
+  function transferAsset(
+    address assetId,
+    address payable recipient,
+    uint256 maxAmount
+  ) internal returns (bool) {
+    (bool success, uint256 amount) = safelyTransfer(assetId, recipient, maxAmount);
+
+    if (success) {
+      registerTransfer(assetId, amount);
+    } else {
+      addToEmergencyWithdrawableAmount(assetId, recipient, maxAmount);
     }
 
-    function safelyTransferERC20(address assetId, address recipient, uint256 maxAmount)
-        private
-        returns (bool, uint256)
-    {
-        (bool success, bytes memory encodedReturnValue) = address(this).call{gas: ERC20_BALANCE_GAS_LIMIT}(
-            abi.encodeWithSignature("_getOwnERC20Balance(address)", assetId)
-        );
-        if (!success) { return (false, 0); }
+    return success;
+  }
 
-        uint256 balance = abi.decode(encodedReturnValue, (uint256));
-        uint256 amount = LibUtils.min(maxAmount, balance);
-        (success, ) = address(this).call{gas: ERC20_TRANSFER_GAS_LIMIT}(
-            abi.encodeWithSignature("_transferERC20(address,address,uint256)", assetId, recipient, amount)
-        );
-        return (success, success ? amount : 0);
-    }
+  function getTotalTransferred(address assetId) external override view onlyOnProxy returns (uint256) {
+    return totalTransferred[assetId];
+  }
 
-    function safelyTransfer(address assetId, address payable recipient, uint256 maxAmount)
-        private
-        returns (bool, uint256)
-    {
-        return LibAsset.isEther(assetId) ?
-            safelyTransferEther(recipient, maxAmount) :
-            safelyTransferERC20(assetId, recipient, maxAmount);
+  function getEmergencyWithdrawableAmount(address assetId, address owner)
+    external
+    override
+    view
+    onlyOnProxy
+    returns (uint256)
+  {
+    return emergencyWithdrawableAmount[assetId][owner];
+  }
 
-    }
+  function emergencyWithdraw(
+    address assetId,
+    address owner,
+    address payable recipient
+  ) external override onlyOnProxy {
+    require(
+      msg.sender == owner || owner == recipient,
+      "AssetTransfer: Either msg.sender or recipient of funds must be the owner of an emergency withdraw"
+    );
 
-    function _getOwnERC20Balance(address assetId)
-        external
-        onlySelf
-        view
-        returns (uint256)
-    {
-        return IERC20(assetId).balanceOf(address(this));
-    }
+    uint256 maxAmount = emergencyWithdrawableAmount[assetId][owner];
+    uint256 balance = LibAsset.getOwnBalance(assetId);
+    uint256 amount = LibUtils.min(maxAmount, balance);
 
-    function _transferERC20(address assetId, address recipient, uint256 amount)
-        external
-        onlySelf
-        returns (bool)
-    {
-        return LibAsset.transferERC20(assetId, recipient, amount);
-    }
-
-    function registerTransfer(address assetId, uint256 amount)
-        internal
-    {
-        totalTransferred[assetId] += amount;
-    }
-
-    function addToEmergencyWithdrawableAmount(address assetId, address owner, uint256 amount)
-        internal
-    {
-        emergencyWithdrawableAmount[assetId][owner] += amount;
-    }
-
-    function transferAsset(address assetId, address payable recipient, uint256 maxAmount)
-        internal
-        returns (bool)
-    {
-        (bool success, uint256 amount) = safelyTransfer(assetId, recipient, maxAmount);
-
-        if (success) {
-            registerTransfer(assetId, amount);
-        } else {
-            addToEmergencyWithdrawableAmount(assetId, recipient, maxAmount);
-        }
-
-        return success;
-    }
-
-    function getTotalTransferred(address assetId)
-        external
-        override
-        view
-        onlyOnProxy
-        returns (uint256)
-    {
-        return totalTransferred[assetId];
-    }
-
-    function getEmergencyWithdrawableAmount(address assetId, address owner)
-        external
-        override
-        view
-        onlyOnProxy
-        returns (uint256)
-    {
-        return emergencyWithdrawableAmount[assetId][owner];
-    }
-
-    function emergencyWithdraw(address assetId, address owner, address payable recipient)
-        external
-        override
-        onlyOnProxy
-    {
-        require(
-            msg.sender == owner || owner == recipient,
-            "AssetTransfer: Either msg.sender or recipient of funds must be the owner of an emergency withdraw"
-        );
-
-        uint256 maxAmount = emergencyWithdrawableAmount[assetId][owner];
-        uint256 balance = LibAsset.getOwnBalance(assetId);
-        uint256 amount = LibUtils.min(maxAmount, balance);
-
-        emergencyWithdrawableAmount[assetId][owner] = emergencyWithdrawableAmount[assetId][owner].sub(amount);
-        registerTransfer(assetId, maxAmount);
-        require(
-            LibAsset.transfer(assetId, recipient, amount),
-            "AssetTransfer: Transfer failed"
-        );
-    }
-
+    emergencyWithdrawableAmount[assetId][owner] = emergencyWithdrawableAmount[assetId][owner].sub(amount);
+    registerTransfer(assetId, maxAmount);
+    require(LibAsset.transfer(assetId, recipient, amount), "AssetTransfer: Transfer failed");
+  }
 }
