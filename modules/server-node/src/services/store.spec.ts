81d377374da5682a3bd1c85bace434fcb0a22197
bohendo <bohende@gmail.com>
2020-10-28 18:59:29 +0530

    ❌ add .only to contract,server-node,duet tests

--- modules/server-node/src/services/store.spec.ts
+++ modules/server-node/src/services/store.spec.ts
@@ -28,7 +28,7 @@ import { config } from "../config";
 
 import { PrismaStore } from "./store";
 
-describe("store", () => {
+describe.only("store", () => {
   let store: PrismaStore;
 
   before(() => {
