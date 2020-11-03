e600eb5c73659066d8bf9c42d8ebe25f8b9282b0
LayneHaber <layne.haber@gmail.com>
2020-11-02 21:11:45 -0700

    tests hit different eth networks

--- modules/contracts/hardhat.config.ts
+++ modules/contracts/hardhat.config.ts
@@ -8,7 +8,11 @@ import * as packageJson from "./package.json";
 
 // create accounts with the default balance of MAX_INT / 2
 // and use them to fund accounts in the test as needed
-const MAX_INT = BigNumber.from(2).pow(256).sub(1);
+const MAX_INT = BigNumber.from(2)
+  .pow(256)
+  .sub(1);
+
+const DEFAULT_MNEMONIC = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 import "@nomiclabs/hardhat-waffle";
 
@@ -36,38 +40,15 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1338,
       loggingEnabled: false,
-      accounts: [
-        {
-          // 0xebb77dCE22ae0f9003359B7f7fe7b7eA0034529d
-          privateKey: "0xf8db28f19cfb75625e0c100de3de8be364f2f4a6d77ff3b3ea361b93bef625dd",
-          balance: MAX_INT.div(2).toString(),
-        },
-        {
-          // 0xbBFeca66860d78Eb9d037B0F9F6093025EF096A3
-          privateKey: "0xbce6e7f2cbb131f5538b052f433b381c0738d37c3df2d667d023ee10adbb33f0",
-          balance: MAX_INT.div(2).toString(),
-        },
-        {
-          // 0x9f2Acf6dd8D083B9688113e8ee5DEfC3906ee7D8
-          privateKey: "0x5454ba77acd18c6cef9dd471a7bc57d8ff261433a2c2d90049659fe68eaf1de4",
-          balance: MAX_INT.div(2).toString(),
-        },
-        {
-          // 0x7bf6714413b8829c470d549dA07B6338D0de4142
-          privateKey: "0x615ff2525e11be7b323e699e720378641ea2c418d829d065e74d1fd70a44706b",
-          balance: MAX_INT.div(2).toString(),
-        },
-        {
-          // 0x691F096377eD5C63e3f43b0903EFd7a34CcC23Bf
-          privateKey: "0x20a88167e85946376ba44cf26f347c2f6d3d4f6e3302bda1990355d267c22051",
-          balance: MAX_INT.div(2).toString(),
-        },
-        {
-          // 0x627306090abaB3A6e1400e9345bC60c78a8BEf57 // SUGAR DADDY
-          privateKey: "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
-          balance: MAX_INT.div(2).toString(),
-        },
-      ],
+      accounts: {
+        mnemonic: process.env.SUGAR_DADDY ?? process.env.mnemonic ?? DEFAULT_MNEMONIC,
+        accountsBalance: MAX_INT.div(2).toString(),
+      },
+    },
+    matic: {
+      chainId: 80001,
+      url: "https://rpc-mumbai.matic.today",
+      accounts: { mnemonic: process.env.SUGAR_DADDY ?? process.env.mnemonic ?? DEFAULT_MNEMONIC },
     },
   },
 };
--- modules/contracts/src.ts/tests/constants.ts
+++ modules/contracts/src.ts/tests/constants.ts
@@ -1,8 +1,37 @@
-import { waffle } from "hardhat";
+import { expect } from "chai";
+import { utils, Wallet } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import * as hardhat from "hardhat";
 
-export const provider = waffle.provider;
-export const chainIdReq = provider.getNetwork().then((net) => net.chainId);
-export const wallets = provider.getWallets();
+const MIN_BALANCE = parseEther("0.1");
+export const provider = hardhat.waffle.provider;
+const hdNode = utils.HDNode.fromMnemonic(process.env.SUGAR_DADDY!).derivePath("m/44'/60'/0'/0");
+export const wallets: Wallet[] = Array(20)
+  .fill(0)
+  .map((_, idx) => {
+    const wallet = new Wallet(hdNode.derivePath(idx.toString()).privateKey, provider);
+    return wallet;
+  });
+
+export const chainIdReq = provider.getNetwork().then(net => net.chainId);
 export const alice = wallets[0];
 export const bob = wallets[1];
 export const rando = wallets[2];
+
+before(async () => {
+  let balance = await provider.getBalance(alice.address);
+  expect(balance).to.be.gte(
+    MIN_BALANCE,
+    `Expected ${alice.address} to have at least ${MIN_BALANCE.toString()} (current balance: ${balance.toString()})`,
+  );
+  balance = await provider.getBalance(bob.address);
+  expect(balance).to.be.gte(
+    MIN_BALANCE,
+    `Expected ${bob.address} to have at least ${MIN_BALANCE.toString()} (current balance: ${balance.toString()})`,
+  );
+  balance = await provider.getBalance(rando.address);
+  expect(balance).to.be.gte(
+    MIN_BALANCE,
+    `Expected ${rando.address} to have at least ${MIN_BALANCE.toString()} (current balance: ${balance.toString()})`,
+  );
+});
