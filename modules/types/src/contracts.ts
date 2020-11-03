d408683e975e456ee55ec57bfcce2eb3902f8e05
Heiko Fisch <heiko.fisch@gmail.com>
2020-11-04 00:43:37 +0100

    Introduce `WithdrawDataEncoding` in types

--- modules/types/src/contracts.ts
+++ modules/types/src/contracts.ts
@@ -4,3 +4,14 @@ export const BalanceEncoding = tidy(`tuple(
     uint256[2] amount,
     address[2] to
   )`);
+
+export const WithdrawDataEncoding = tidy(`tuple(
+    address channelAddress,
+    address assetId,
+    address recipient,
+    uint256 amount,
+    uint256 nonce,
+    address callTo,
+    uint256 callValue,
+    bytes callData
+  )`);
