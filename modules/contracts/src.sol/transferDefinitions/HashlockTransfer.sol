ad1b696dec33bee51b23ee45d16a2ca32a4efc3b
Heiko Fisch <heiko.fisch@gmail.com>
2020-11-06 22:26:15 +0100

    Change order of checks
    This order should allow the HashlockTransfer to work with pure-evm when
    expiry is 0.

--- modules/contracts/src.sol/transferDefinitions/HashlockTransfer.sol
+++ modules/contracts/src.sol/transferDefinitions/HashlockTransfer.sol
@@ -41,7 +41,7 @@ contract HashlockTransfer is ITransferDefinition {
 
     require(balance.amount[1] == 0, "Cannot create hashlock transfer with nonzero recipient balance");
     require(state.lockHash != bytes32(0), "Cannot create hashlock transfer with empty lockHash");
-    require(state.expiry > block.number || state.expiry == 0, "Cannot create hashlock transfer with expired timelock");
+    require(state.expiry == 0 || state.expiry > block.number, "Cannot create hashlock transfer with expired timelock");
     return true;
   }
 
@@ -56,7 +56,7 @@ contract HashlockTransfer is ITransferDefinition {
 
     // If you pass in bytes32(0), payment is canceled
     // If timelock is nonzero and has expired, payment is canceled
-    if (resolver.preImage != bytes32(0) && (state.expiry > block.number || state.expiry == 0)) {
+    if (resolver.preImage != bytes32(0) && (state.expiry == 0 || state.expiry > block.number)) {
       // Check hash for normal payment unlock
       bytes32 generatedHash = sha256(abi.encode(resolver.preImage));
       require(state.lockHash == generatedHash, "Hash generated from preimage does not match hash in state");
