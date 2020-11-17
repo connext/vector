b94cbd5e585df5dcbe38ac94fcb83bf0af4cde26
LayneHaber <layne.haber@gmail.com>
2020-11-16 19:02:07 -0700

    Remove setup

--- modules/contracts/src.sol/ReentrancyGuard.sol
+++ modules/contracts/src.sol/ReentrancyGuard.sol
@@ -4,28 +4,20 @@ pragma experimental ABIEncoderV2;
 
 // A "mutex" reentrancy guard, heavily influenced by OpenZeppelin's
 contract ReentrancyGuard {
+  uint256 private constant OPEN = 0;
+  uint256 private constant LOCKED = 1;
 
-    uint256 private constant UNINITIALIZED = 0;
-    uint256 private constant OPEN = 1;
-    uint256 private constant LOCKED = 2;
+  uint256 public lock;
 
-    uint256 public lock;
+  modifier nonReentrant() {
+    require(lock == OPEN, "ReentrancyGuard: REENTRANT_CALL");
+    lock = LOCKED;
+    _;
+    lock = OPEN;
+  }
 
-    function setup() internal {
-        require(lock == UNINITIALIZED, "ReentrancyGuard: ALREADY_INITIALIZED");
-        lock = OPEN;
-    }
-
-    modifier nonReentrant() {
-        require(lock == OPEN, "ReentrancyGuard: REENTRANT_CALL");
-        lock = LOCKED;
-        _;
-        lock = OPEN;
-    }
-
-    modifier nonReentrantView() {
-        require(lock == OPEN, "ReentrancyGuard: REENTRANT_CALL");
-        _;
-    }
-
+  modifier nonReentrantView() {
+    require(lock == OPEN, "ReentrancyGuard: REENTRANT_CALL");
+    _;
+  }
 }
