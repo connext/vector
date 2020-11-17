b5b4b7479e8bc15c40e3b31ab15e1f02680a560c
LayneHaber <layne.haber@gmail.com>
2020-11-16 17:03:30 -0700

    whitespace

--- modules/contracts/src.sol/interfaces/IChannelFactory.sol
+++ modules/contracts/src.sol/interfaces/IChannelFactory.sol
@@ -3,29 +3,29 @@ pragma solidity ^0.7.1;
 pragma experimental ABIEncoderV2;
 
 interface IChannelFactory {
-    event ChannelCreation(address channel);
+  event ChannelCreation(address channel);
 
-    function getMastercopy() external view returns (address);
+  function getMastercopy() external view returns (address);
 
-    function proxyCreationCode() external view returns (bytes memory);
+  function proxyCreationCode() external view returns (bytes memory);
 
-    function getChannelAddress(
-      address alice,
-      address bob,
-      uint256 chainId
-    ) external view returns (address);
+  function getChannelAddress(
+    address alice,
+    address bob,
+    uint256 chainId
+  ) external view returns (address);
 
-    function createChannel(
-      address alice,
-      address bob,
-      uint256 chainId
-    ) external returns (address);
+  function createChannel(
+    address alice,
+    address bob,
+    uint256 chainId
+  ) external returns (address);
 
-    function createChannelAndDepositAlice(
-      address alice,
-      address bob,
-      uint256 chainId,
-      address assetId,
-      uint256 amount
-    ) external payable returns (address);
+  function createChannelAndDepositAlice(
+    address alice,
+    address bob,
+    uint256 chainId,
+    address assetId,
+    uint256 amount
+  ) external payable returns (address);
 }
