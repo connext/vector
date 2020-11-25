d7c25b6c026ee6e66d67b8825be3750f243612bb
LayneHaber <layne.haber@gmail.com>
2020-11-24 19:58:35 -0700

    use chainId

--- modules/utils/src/eth.ts
+++ modules/utils/src/eth.ts
@@ -7,11 +7,11 @@ const classicProviders = ["https://www.ethercluster.com/etc"];
 const classicChainIds = [61];
 const minGasPrice = BigNumber.from(1_000);
 
-export const getEthProvider = (providerUrl: string, chainId?: number): JsonRpcProvider =>
-  new JsonRpcProvider(
-    providerUrl,
-    classicProviders.includes(providerUrl) || classicChainIds.includes(chainId) ? "classic" : undefined,
-  );
+export const getEthProvider = (providerUrl: string, chainId?: number): JsonRpcProvider => {
+  const networkInfo =
+    classicProviders.includes(providerUrl) || classicChainIds.includes(chainId) ? "classic" : undefined;
+  return new JsonRpcProvider(providerUrl, chainId ?? networkInfo);
+};
 
 // xDai hardcoded their gas price to 0 but it's not actually zero..
 export const getGasPrice = async (provider: Provider, providedChainId?: number): Promise<BigNumber> => {
