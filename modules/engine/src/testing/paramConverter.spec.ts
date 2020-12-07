f117332e255efb3367c3e26ebe73bfca8d3faaea
LayneHaber <layne.haber@gmail.com>
2020-12-06 20:19:37 -0700

    Fix engine tests

--- modules/engine/src/testing/paramConverter.spec.ts
+++ modules/engine/src/testing/paramConverter.spec.ts
@@ -1,5 +1,4 @@
 import {
-  ChainAddresses,
   CreateTransferParams,
   DEFAULT_TRANSFER_TIMEOUT,
   EngineParams,
@@ -11,6 +10,7 @@ import {
   Result,
   TransferNames,
   ChainError,
+  UpdateType,
 } from "@connext/vector-types";
 import {
   createTestChannelState,
@@ -41,12 +41,7 @@ describe("ParamConverter", () => {
   const providerUrl = env.chainProviders[chainId];
   const signerA = getRandomChannelSigner(providerUrl);
   const signerB = getRandomChannelSigner(providerUrl);
-  const chainAddresses: ChainAddresses = {
-    [chainId]: {
-      channelFactoryAddress: env.chainAddresses[chainId].channelFactoryAddress,
-      transferRegistryAddress: env.chainAddresses[chainId].transferRegistryAddress,
-    },
-  };
+  const chainAddresses = { ...env.chainAddresses };
   const withdrawRegisteredInfo: RegisteredTransfer = {
     definition: mkAddress("0xdef"),
     resolverEncoding: "resolve",
@@ -63,7 +58,6 @@ describe("ParamConverter", () => {
 
   beforeEach(() => {
     chainReader = Sinon.createStubInstance(VectorChainReader);
-
     chainReader.getBlockNumber.resolves(Result.ok<number>(110));
   });
 
@@ -99,7 +93,7 @@ describe("ParamConverter", () => {
 
     it("should work for A", async () => {
       const params = generateParams();
-      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], "setup", {
+      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], UpdateType.deposit, {
         channelAddress: params.channelAddress,
         networkContext: {
           ...chainAddresses[chainId],
@@ -140,7 +134,7 @@ describe("ParamConverter", () => {
 
     it("should work for B", async () => {
       const params = generateParams();
-      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], "setup", {
+      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], UpdateType.deposit, {
         channelAddress: params.channelAddress,
         networkContext: {
           ...chainAddresses[chainId],
@@ -184,7 +178,7 @@ describe("ParamConverter", () => {
       const params: any = generateParams();
       // Set incorrect type
       params.conditionType = "FailingTest";
-      const channelState: FullChannelState = createTestChannelState("setup", {
+      const channelState: FullChannelState = createTestChannelState(UpdateType.deposit, {
         channelAddress: params.channelAddress,
         networkContext: {
           ...chainAddresses[chainId],
@@ -265,7 +259,7 @@ describe("ParamConverter", () => {
 
     it("should work for A", async () => {
       const params = generateParams();
-      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], "setup", {
+      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], UpdateType.deposit, {
         channelAddress: params.channelAddress,
         networkContext: {
           ...chainAddresses[chainId],
@@ -306,7 +300,7 @@ describe("ParamConverter", () => {
 
     it("should work for B", async () => {
       const params = generateParams();
-      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], "setup", {
+      const channelState: FullChannelState = createTestChannelStateWithSigners([signerA, signerB], UpdateType.deposit, {
         channelAddress: params.channelAddress,
         networkContext: {
           ...chainAddresses[chainId],
