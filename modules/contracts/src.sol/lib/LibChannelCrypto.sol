d9c1bb2af8934fb436196558dff0df2d787da07f
Rahul Sethuram <rahul@sethuram.com>
2020-12-10 09:52:37 +0100

    Add utility message fns to LibChannelCrypto.sol

--- modules/contracts/src.sol/lib/LibChannelCrypto.sol
+++ modules/contracts/src.sol/lib/LibChannelCrypto.sol
@@ -4,12 +4,6 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/cryptography/ECDSA.sol";
 
-/// @title LibChannelCrypto
-/// @author Connext <support@connext.network>
-/// @notice This library contains helpers for recovering signatures from a
-///         Vector commitments. Channels do not allow for arbitrary signing of
-///         messages to prevent misuse of private keys by injected providers,
-///         and instead only sign messages with a Vector channel prefix.
 library LibChannelCrypto {
     function checkSignature(
         bytes32 hash,
@@ -35,8 +29,35 @@ library LibChannelCrypto {
     {
         // 32 is the length in bytes of hash,
         // enforced by the type signature above
-        // TODO namespace this to Vector?
         return
-            keccak256(abi.encodePacked("\x15Vector Signed Message:\n32", hash));
+            keccak256(abi.encodePacked("\x15Indra Signed Message:\n32", hash));
+    }
+
+    function checkUtilitySignature(
+        bytes32 hash,
+        bytes memory signature,
+        address allegedSigner
+    ) internal pure returns (bool) {
+        return recoverChannelMessageSigner(hash, signature) == allegedSigner;
+    }
+
+    function recoverUtilityMessageSigner(bytes32 hash, bytes memory signature)
+        internal
+        pure
+        returns (address)
+    {
+        bytes32 digest = toUtilitySignedMessage(hash);
+        return ECDSA.recover(digest, signature);
+    }
+
+    function toUtilitySignedMessage(bytes32 hash)
+        internal
+        pure
+        returns (bytes32)
+    {
+        // 32 is the length in bytes of hash,
+        // enforced by the type signature above
+        return
+            keccak256(abi.encodePacked("\x15Utility Signed Message:\n32", hash));
     }
 }
