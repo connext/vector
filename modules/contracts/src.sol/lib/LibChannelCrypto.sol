da149112b7926b7f502f8c46368a3af6466c04df
Rahul Sethuram <rahul@sethuram.com>
2020-12-10 09:55:25 +0100

    Add back comment to lib

--- modules/contracts/src.sol/lib/LibChannelCrypto.sol
+++ modules/contracts/src.sol/lib/LibChannelCrypto.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.7.1;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/cryptography/ECDSA.sol";
-
+		
+/// @author Connext <support@connext.network>		
+/// @notice This library contains helpers for recovering signatures from a		
+///         Vector commitments. Channels do not allow for arbitrary signing of		
+///         messages to prevent misuse of private keys by injected providers,		
+///         and instead only sign messages with a Vector channel prefix.
 library LibChannelCrypto {
     function checkSignature(
         bytes32 hash,
