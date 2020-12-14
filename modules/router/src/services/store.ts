0622bb94effea68e54854400e33575307b37cd0b
Rahul Sethuram <rahul@sethuram.com>
2020-12-14 15:42:20 +0100

    Typescript God mode

--- modules/router/src/services/store.ts
+++ modules/router/src/services/store.ts
@@ -1,7 +1,57 @@
-export interface IRouterStore {
-  queueUpdate(type: string, params: any): Promise<void>;
-}
+import { NodeParams } from "@connext/vector-types";
 
+export const RouterUpdateStatus = {
+  PENDING: "PENDING",
+  COMPLETE: "COMPLETE",
+  FAILED: "FAILED",
+} as const;
+export type RouterUpdateStatus = keyof typeof RouterUpdateStatus;
+
+export const RouterUpdateType = {
+  TRANSFER_CREATION: "TRANSFER_CREATION",
+  TRANSFER_RESOLUTION: "TRANSFER_RESOLUTION",
+} as const;
+export type RouterUpdateType = keyof typeof RouterUpdateType;
+
+export type RouterStoredUpdatePayload = {
+  [RouterUpdateType.TRANSFER_CREATION]: NodeParams.ConditionalTransfer;
+  [RouterUpdateType.TRANSFER_RESOLUTION]: NodeParams.ResolveTransfer;
+};
+
+export type RouterStoredUpdate<T extends RouterUpdateType> = {
+  id: string;
+  type: T;
+  payload: RouterStoredUpdatePayload[T];
+};
+export interface IRouterStore {
+  getQueuedUpdates(
+    channelAddress: string,
+    status?: RouterUpdateStatus,
+  ): Promise<RouterStoredUpdate<RouterUpdateType>[]>;
+  queueUpdate<T extends RouterUpdateType>(
+    channelAddress: string,
+    type: T,
+    updateData: RouterStoredUpdatePayload[T],
+  ): Promise<void>;
+  setUpdateStatus(updateId: string, status: RouterUpdateStatus, context?: string): Promise<void>;
+}
 export class RouterStore implements IRouterStore {
-  async queueUpdate(type: string, params: any): Promise<void> {}
+  async getQueuedUpdates(
+    channelAddress: string,
+    status?: RouterUpdateStatus,
+  ): Promise<RouterStoredUpdate<RouterUpdateType>[]> {
+    return [];
+  }
+
+  async queueUpdate<T extends RouterUpdateType>(
+    channelAddress: string,
+    type: T,
+    updateData: RouterStoredUpdatePayload[T],
+  ): Promise<void> {
+    return;
+  }
+
+  async setUpdateStatus(updateId: string, status: RouterUpdateStatus, context?: string): Promise<void> {
+    return;
+  }
 }
