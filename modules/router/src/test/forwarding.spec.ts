1333a32e0b71e7201fd3772063114cf6cbe34b4a
LayneHaber <layne.haber@gmail.com>
2020-12-17 17:20:26 -0700

    refactor forwarding

--- modules/router/src/test/forwarding.spec.ts
+++ modules/router/src/test/forwarding.spec.ts
@@ -1,65 +1,155 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { ConditionalTransferCreatedPayload, INodeService, Result, TransferNames } from "@connext/vector-types";
+import {
+  ConditionalTransferCreatedPayload,
+  INodeService,
+  Result,
+  TransferNames,
+  FullChannelState,
+  FullTransferState,
+  UpdateType,
+  TRANSFER_DECREMENT,
+} from "@connext/vector-types";
 import {
   createTestChannelState,
-  createTestFullHashlockTransferState,
   expect,
   getRandomBytes32,
   mkAddress,
   mkPublicIdentifier,
   RestServerNodeService,
+  getTestLoggers,
 } from "@connext/vector-utils";
-import { AddressZero } from "@ethersproject/constants";
-import pino from "pino";
+import { BigNumber } from "@ethersproject/bignumber";
 import Sinon from "sinon";
 
 import { RouterStore } from "../services/store";
-import { forwardTransferCreation } from "../forwarding";
+import { forwardTransferCreation, ForwardTransferError } from "../forwarding";
 import { config } from "../config";
+import * as swapService from "../services/swap";
+import * as collateral from "../collateral";
 
 import { mockProvider } from "./utils/mocks";
 
-const hydratedProviders = { 1337: mockProvider };
+const testName = "Forwarding";
 
-const logger = pino({ level: config.logLevel });
+const { log: logger } = getTestLoggers(testName, config.logLevel as any);
 
-describe("Forwarding", () => {
-  describe("transferCreation", () => {
+type TransferCreatedTestContext = {
+  senderTransfer: FullTransferState;
+  senderChannel: FullChannelState;
+  receiverChannel: FullChannelState;
+  event: ConditionalTransferCreatedPayload;
+};
+
+describe.only("Forwarding", () => {
+  describe("forwardTransferCreation", () => {
     let node: Sinon.SinonStubbedInstance<RestServerNodeService>;
     let store: Sinon.SinonStubbedInstance<RouterStore>;
+    let getSwappedAmount: Sinon.SinonStub;
+    let requestCollateral: Sinon.SinonStub;
 
-    const generateTransferData = (): ConditionalTransferCreatedPayload => {
-      const channelAddress = mkAddress("0x1");
-      const routingId = getRandomBytes32();
-      const channelBalance = {
-        amount: ["5", "7"],
-        to: [mkAddress("0xa"), mkAddress("0xb")],
+    const routerPublicIdentifier = mkPublicIdentifier("vectorRRR");
+    const aliceIdentifier = mkPublicIdentifier("vectorA");
+    const bobIdentifier = mkPublicIdentifier("vectorB");
+    const signerAddress = mkAddress("0xBBB");
+    const chainProviders = { 1337: mockProvider };
+    const testLog = logger.child({ module: "forwardTransferCreation" });
+
+    const generateDefaultTestContext = (): TransferCreatedTestContext => {
+      const transferMeta = {
+        routingId: getRandomBytes32(),
+        path: [{ recipient: bobIdentifier }],
       };
-      const transfer = createTestFullHashlockTransferState({
-        channelAddress,
-        balance: {
-          amount: ["2", "0"],
-          to: channelBalance.to,
-        },
-        assetId: mkAddress("0x0"),
-        meta: { routingId, path: [channelAddress] },
+      const { channel: senderChannel, transfer: senderTransfer } = createTestChannelState(
+        UpdateType.create,
+        { aliceIdentifier: routerPublicIdentifier, bobIdentifier: aliceIdentifier, alice: signerAddress },
+        { meta: transferMeta },
+      );
+
+      const { channel: receiverChannel } = createTestChannelState(UpdateType.deposit, {
+        aliceIdentifier: routerPublicIdentifier,
+        bobIdentifier,
+        alice: signerAddress,
       });
-      const conditionType = TransferNames.HashlockTransfer;
-      return {
-        channelAddress,
-        transfer,
-        channelBalance,
-        conditionType,
-        aliceIdentifier: mkPublicIdentifier("vectorA"),
-        bobIdentifier: mkPublicIdentifier("vectorB"),
+
+      const idx = senderChannel.assetIds.findIndex((a) => a === senderTransfer.assetId);
+
+      const event: ConditionalTransferCreatedPayload = {
+        aliceIdentifier: senderChannel.aliceIdentifier,
+        bobIdentifier: senderChannel.bobIdentifier,
+        channelAddress: senderChannel.channelAddress,
+        channelBalance: senderChannel.balances[idx],
+        activeTransferIds: [senderTransfer.transferId],
+        transfer: senderTransfer,
+        conditionType: TransferNames.HashlockTransfer,
       };
+      return { event, senderTransfer, senderChannel, receiverChannel };
+    };
+
+    const prepEnv = (ctx: TransferCreatedTestContext = generateDefaultTestContext()): TransferCreatedTestContext => {
+      const { receiverChannel, senderChannel, senderTransfer } = ctx;
+
+      // Set mock methods for default happy case
+      // get sender channel
+      node.getStateChannel.onFirstCall().resolves(Result.ok(senderChannel));
+      // get swapped amount (optional)
+      getSwappedAmount.resolves(Result.ok(senderTransfer.balance.amount[0]));
+      // get receiver channel
+      node.getStateChannelByParticipants.onFirstCall().resolves(Result.ok(receiverChannel));
+      // request collateral (optional)
+      requestCollateral.resolves(Result.ok({ channelAddress: receiverChannel.channelAddress }));
+      // create receiver transfer
+      node.conditionalTransfer.onFirstCall().resolves(
+        Result.ok({
+          channelAddress: receiverChannel.channelAddress,
+          transferId: getRandomBytes32(),
+          routingId: senderTransfer.meta.routingId,
+        }),
+      );
+
+      return ctx;
+    };
+
+    const verifySuccessfulResult = (
+      result: Result<any, ForwardTransferError>,
+      ctx: TransferCreatedTestContext,
+      swapCallCount = 0,
+      collateralCallCount = 0,
+    ) => {
+      const { senderTransfer, receiverChannel, event } = ctx;
+      expect(result.getError()).to.be.undefined;
+      expect(result.getValue()).to.containSubset({
+        channelAddress: receiverChannel.channelAddress,
+        routingId: senderTransfer.meta.routingId,
+      });
+      expect(result.getValue().transferId).to.be.ok;
+      // Verify call stack
+      expect(getSwappedAmount.callCount).to.be.eq(swapCallCount);
+      expect(requestCollateral.callCount).to.be.eq(collateralCallCount);
+      expect(node.conditionalTransfer.callCount).to.be.eq(1);
+      expect(
+        node.conditionalTransfer.calledOnceWithExactly({
+          channelAddress: receiverChannel.channelAddress,
+          amount: swapCallCount > 0 ? getSwappedAmount.returnValues[0].getValue() : senderTransfer.balance.amount[0],
+          assetId: senderTransfer.meta.path.recipientAssetId ?? senderTransfer.assetId,
+          timeout: BigNumber.from(senderTransfer.transferTimeout).sub(TRANSFER_DECREMENT).toString(),
+          type: event.conditionType,
+          publicIdentifier: routerPublicIdentifier,
+          details: { ...senderTransfer.transferState },
+          meta: {
+            ...(senderTransfer.meta ?? {}),
+          },
+        }),
+      );
     };
 
     beforeEach(async () => {
+      // Declare stubs
       node = Sinon.createStubInstance(RestServerNodeService, {
         sendDepositTx: Promise.resolve(Result.ok({ txHash: getRandomBytes32() })),
       });
       store = Sinon.createStubInstance(RouterStore);
+      getSwappedAmount = Sinon.stub(swapService, "getSwappedAmount");
+      requestCollateral = Sinon.stub(collateral, "requestCollateral");
     });
 
     afterEach(() => {
@@ -67,41 +157,19 @@ describe("Forwarding", () => {
       Sinon.reset();
     });
 
-    it("successfully forwards a transfer creation with no swaps, no cross-chain and no collateralization", async () => {
-      const data = generateTransferData();
-      const senderChannel = createTestChannelState("create", {
-        alice: mkAddress("0xa"),
-        bob: mkAddress("0xb1"),
-        channelAddress: data.channelAddress,
-        balances: [data.channelBalance],
-      }).channel;
-      const receiverChannel = createTestChannelState("deposit", {
-        alice: mkAddress("0xa"),
-        bob: mkAddress("0xb2"),
-        assetIds: [AddressZero],
-        balances: [
-          {
-            amount: ["5", "7"],
-            to: [mkAddress("0xb"), mkAddress("0xc")],
-          },
-        ],
-      }).channel;
-      node.getStateChannel.resolves(Result.ok(senderChannel));
-      node.getStateChannelByParticipants.resolves(Result.ok(receiverChannel));
-      node.conditionalTransfer.resolves(Result.ok({} as any));
-      node.sendDepositTx.resolves(Result.ok({ txHash: getRandomBytes32() }));
-      node.reconcileDeposit.resolves(Result.ok({ channelAddress: data.channelAddress }));
-      await expect(
-        forwardTransferCreation(
-          data,
-          mkPublicIdentifier("vectorBBB"),
-          mkAddress("0xb"),
-          node as INodeService,
-          store,
-          logger,
-          hydratedProviders,
-        ),
-      ).to.eventually.be.ok;
+    it.only("successfully forwards a transfer creation with no swaps, no cross-chain and no collateralization", async () => {
+      const ctx = prepEnv();
+      const result = await forwardTransferCreation(
+        ctx.event,
+        routerPublicIdentifier,
+        signerAddress,
+        node as INodeService,
+        store,
+        testLog,
+        chainProviders,
+      );
+
+      verifySuccessfulResult(result, ctx);
     });
 
     it.skip("successfully forwards a transfer creation with swaps, no cross-chain and no collateralization", async () => {});
