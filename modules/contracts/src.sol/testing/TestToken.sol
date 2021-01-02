d9c6fe061001160d79dbf08ca33ba879ce7b5082
LayneHaber <layne.haber@gmail.com>
2021-01-02 15:09:07 -0700

    token updates

--- modules/contracts/src.sol/testing/TestToken.sol
+++ modules/contracts/src.sol/testing/TestToken.sol
@@ -3,21 +3,26 @@
 pragma solidity ^0.7.1;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /* This token is ONLY useful for testing
  * Anybody can mint as many tokens as they like
  * Anybody can burn anyone else's tokens
  */
-contract TestToken is ERC20 {
+contract TestToken is ERC20, Ownable {
     constructor() ERC20("Test Token", "TEST") {
         _mint(msg.sender, 1000000 ether);
     }
 
-    function mint(address account, uint256 amount) external {
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) external {
+    function drip(address account) external {
+        _mint(account, 1 ether);
+    }
+
+    function burn(address account, uint256 amount) external onlyOwner {
         _burn(account, amount);
     }
 }
