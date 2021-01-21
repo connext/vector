7fa308395532ac74f9728fb90f082c782b556aa6
Rahul Sethuram <rahul@sethuram.com>
2021-01-21 21:37:05 +0100

    More gRPC work

--- modules/server-node/src/grpc/server.ts
+++ modules/server-node/src/grpc/server.ts
@@ -4,7 +4,10 @@
 // https://github.com/timostamm/protobuf-ts/tree/master/packages/example-node-grpc-server
 
 import * as grpc from "@grpc/grpc-js";
-import { assert } from "@protobuf-ts/runtime";
+import { jsonifyError } from "@connext/vector-types";
+
+import { logger, store } from "..";
+import { createNode, deleteNodes } from "../helpers/nodes";
 
 import { CreateNodeReply, CreateNodeRequest } from "./gen/vector";
 import { vectorServiceDefinition, IVectorService } from "./gen/vector.grpc-server";
@@ -17,74 +20,25 @@ const vectorService: IVectorService = {
     call: grpc.ServerUnaryCall<CreateNodeRequest, CreateNodeReply>,
     callback: grpc.sendUnaryData<CreateNodeReply>,
   ): Promise<void> {
-    // wait for the requested amount of milliseconds
     try {
       let storedMnemonic = await store.getMnemonic();
-      if (request.body.mnemonic && request.body.mnemonic !== storedMnemonic) {
+      if (call.request.mnemonic && call.request.mnemonic !== storedMnemonic) {
         logger.warn({}, "Mnemonic provided, resetting stored mnemonic");
         // new mnemonic, reset nodes and store mnemonic
         await deleteNodes(store);
-        store.setMnemonic(request.body.mnemonic);
-        storedMnemonic = request.body.mnemonic;
+        store.setMnemonic(call.request.mnemonic);
+        storedMnemonic = call.request.mnemonic;
       }
-      const newNode = await createNode(request.body.index, store, storedMnemonic!, request.body.skipCheckIn ?? false);
-      return reply.status(200).send({
-        index: request.body.index,
+      const newNode = await createNode(call.request.index, store, storedMnemonic!, call.request.skipCheckIn ?? false);
+      callback(null, {
+        index: call.request.index,
         publicIdentifier: newNode.publicIdentifier,
         signerAddress: newNode.signerAddress,
-      } as NodeResponses.CreateNode);
+      });
     } catch (e) {
       logger.error({ error: e.toJson() });
-      return reply.status(500).send(
-        new ServerNodeError(ServerNodeError.reasons.CreateNodeFailed, "", request.body, {
-          createNodeError: e.message,
-          createNodeStack: e.stack,
-        }).toJson(),
-      );
+      callback({ code: grpc.status.INTERNAL, details: JSON.stringify(jsonifyError(e)) });
     }
-    setTimeout(function () {
-      switch (call.request.pleaseFail) {
-        case FailRequest.MESSAGE_THEN_ERROR_STATUS:
-          // does not work, client only receives error
-          callback(
-            {
-              code: grpc.status.RESOURCE_EXHAUSTED,
-              details: "you requested an error",
-            },
-            {
-              answer: `You asked: ${call.request.question}`,
-              yourDeadline: call.getDeadline().toString(),
-              yourFailRequest: call.request.pleaseFail,
-              yourRequestHeaders: {},
-            },
-            trailers,
-          );
-          break;
-
-        case FailRequest.ERROR_STATUS_ONLY:
-          const errorMeta = new grpc.Metadata();
-          errorMeta.add("server-trailer", "created by error response on server");
-          callback({
-            code: grpc.status.RESOURCE_EXHAUSTED,
-            details: "you requested an error, no message",
-            metadata: errorMeta,
-          });
-          break;
-
-        case FailRequest.FAIL_REQUEST_NONE:
-          callback(
-            null,
-            {
-              answer: `You asked: ${call.request.question}`,
-              yourDeadline: call.getDeadline().toString(),
-              yourFailRequest: call.request.pleaseFail,
-              yourRequestHeaders: {},
-            },
-            trailers,
-          );
-          break;
-      }
-    }, call.request.pleaseDelayResponseMs);
   },
   createTransfer: () => undefined,
   deposit: () => undefined,
@@ -113,6 +67,7 @@ const vectorService: IVectorService = {
   setup: () => undefined,
   subscribe: () => undefined,
   transferState: () => undefined,
+  withdraw: () => undefined,
 };
 
 function getServer(): grpc.Server {
