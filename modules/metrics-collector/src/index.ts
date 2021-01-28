0e4288c46e2168657246140a3adb5543f2de720f
Rahul Sethuram <rahul@sethuram.com>
2021-01-28 21:22:33 +0100

    Metrics collector

--- modules/metrics-collector/src/index.ts
+++ modules/metrics-collector/src/index.ts
@@ -7,9 +7,11 @@ import {
   getSignerAddressFromPublicIdentifier,
   hydrateProviders,
 } from "@connext/vector-utils";
-import { HydratedProviders } from "@connext/vector-types";
+import { HydratedProviders, ERC20Abi } from "@connext/vector-types";
+import { AddressZero } from "@ethersproject/constants";
+import { Contract } from "@ethersproject/contracts";
 import { Wallet } from "@ethersproject/wallet";
-import { formatEther } from "@ethersproject/units";
+import { formatEther, formatUnits } from "@ethersproject/units";
 
 import { config } from "./config";
 
@@ -26,6 +28,7 @@ server.register(metricsPlugin, { endpoint: "/metrics", prefix: "collector_" });
 const hydrated: HydratedProviders = hydrateProviders(config.chainProviders);
 
 // create gauge for each rebalanced asset and each native asset for the signer address
+// TODO: maybe want to look into an API rather than blowing up our eth providers? although its not that many calls
 Object.entries(hydrated).forEach(([chainId, provider]) => {
   // base asset
   new Gauge({
@@ -37,7 +40,24 @@ Object.entries(hydrated).forEach(([chainId, provider]) => {
     },
   });
 
-  config.rebalanceProfiles.filter((prof) => prof.chainId === chainId);
+  // get all non-zero addresses
+  const assets = config.rebalanceProfiles
+    .filter((prof) => prof.chainId === chainId && prof.assetId !== AddressZero)
+    .map((p) => p.assetId);
+
+  assets.forEach((assetId) => {
+    const token = new Contract(assetId, ERC20Abi, provider);
+    new Gauge({
+      name: `chain-${chainId}-base-asset`,
+      help: "",
+      async collect() {
+        // TODO: probably get decimals outside of this so it doesnt happen on every call
+        const decimals = await token.decimals();
+        const balance = await token.balanceOf(routerSignerAddress);
+        this.set(parseFloat(formatUnits(balance, decimals)));
+      },
+    });
+  });
 });
 
 server.listen(3000, "0.0.0.0", (err, address) => {
