305604399af95580364279cfb9727dc23bbeed4e
LayneHaber <layne.haber@gmail.com>
2021-01-28 22:59:02 -0700

    WIP: not working

--- modules/metrics-collector/src/index.ts
+++ modules/metrics-collector/src/index.ts
@@ -1,13 +1,14 @@
 import fastify from "fastify";
 import metricsPlugin from "fastify-metrics";
 import pino from "pino";
-import { Gauge } from "prom-client";
+import { Gauge, Registry } from "prom-client";
 import {
   getPublicIdentifierFromPublicKey,
   getSignerAddressFromPublicIdentifier,
   hydrateProviders,
 } from "@connext/vector-utils";
-import { HydratedProviders, ERC20Abi } from "@connext/vector-types";
+import { TestToken } from "@connext/vector-contracts";
+import { HydratedProviders } from "@connext/vector-types";
 import { BigNumber } from "@ethersproject/bignumber";
 import { AddressZero } from "@ethersproject/constants";
 import { Contract } from "@ethersproject/contracts";
@@ -28,15 +29,22 @@ server.register(metricsPlugin, { endpoint: "/metrics", prefix: "collector_" });
 
 const hydrated: HydratedProviders = hydrateProviders(config.chainProviders);
 
+const register = new Registry();
+
 // create gauge for each rebalanced asset and each native asset for the signer address
 // TODO: maybe want to look into an API rather than blowing up our eth providers? although its not that many calls
+const gauges: { [chainId: string]: { [asset: string]: Gauge<string> } } = {};
 Object.entries(hydrated).forEach(([chainId, provider]) => {
   // base asset
-  new Gauge({
+  gauges[chainId] = {};
+  gauges[chainId][AddressZero] = new Gauge({
     name: `chain_${chainId}_base_asset`,
     help: `chain_${chainId}_base_asset_help`,
+    registers: [register],
     async collect() {
       const balance = await provider.getBalance(routerSignerAddress);
+      // NOTE: seems to not be getting scraped at interval
+      console.log("***** calling set on base gauge");
       this.set(parseFloat(formatEther(balance)));
     },
   });
@@ -49,17 +57,20 @@ Object.entries(hydrated).forEach(([chainId, provider]) => {
   const tokens: { [asset: string]: { contract: Contract; decimals?: BigNumber } } = {};
   assets.forEach((asset) => {
     tokens[asset] = {
-      contract: new Contract(asset, ERC20Abi, provider),
+      contract: new Contract(asset, TestToken.abi, provider),
       decimals: undefined,
     };
   });
   assets.forEach((assetId) => {
-    new Gauge({
-      name: `chain_${chainId}_asset`,
-      help: `chain_${chainId}_asset_help`,
+    gauges[chainId][assetId] = new Gauge({
+      name: `chain_${chainId}_asset_${assetId}`,
+      help: `chain_${chainId}_asset_${assetId}_help`,
+      registers: [register],
       async collect() {
-        const decimals = tokens[assetId].decimals ?? (await tokens[assetId].contract.decimals());
+        const decimals = tokens[assetId].decimals ?? (await tokens[assetId].contract.functions.decimals());
         const balance = await tokens[assetId].contract.balanceOf(routerSignerAddress);
+        // NOTE: seems to not be getting scraped at interval
+        console.log("***** calling set on asset gauge");
         this.set(parseFloat(formatUnits(balance, decimals)));
       },
     });
