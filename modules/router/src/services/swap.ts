f5ff1580fbe088531542a4d13d3965f62966922f
LayneHaber <layne.haber@gmail.com>
2021-02-14 18:32:49 -0700

    Dont use inverse

--- modules/router/src/services/swap.ts
+++ modules/router/src/services/swap.ts
@@ -14,26 +14,13 @@ export const getSwappedAmount = (
 ): Result<string, SwapError> => {
   const fromAsset = getAddress(fromAssetId);
   const toAsset = getAddress(toAssetId);
-  let swap = config.allowedSwaps.find(
+  const swap = config.allowedSwaps.find(
     (s) =>
       s.fromAssetId === fromAsset &&
       s.fromChainId === fromChainId &&
       s.toAssetId === toAsset &&
       s.toChainId === toChainId,
   );
-
-  let invert = false;
-  if (!swap) {
-    // search other way around swap
-    swap = config.allowedSwaps.find(
-      (s) =>
-        s.toAssetId === fromAsset &&
-        s.toChainId === fromChainId &&
-        s.fromAssetId === toAsset &&
-        s.fromChainId === toChainId,
-    );
-    invert = true;
-  }
 
   // couldnt find both ways
   if (!swap) {
@@ -44,11 +31,7 @@ export const getSwappedAmount = (
 
   // TODO: decimals
   if (swap.hardcodedRate) {
-    if (invert) {
-      return Result.ok(calculateExchangeAmount(fromAmount, inverse(swap.hardcodedRate)));
-    } else {
-      return Result.ok(calculateExchangeAmount(fromAmount, swap.hardcodedRate));
-    }
+    return Result.ok(calculateExchangeAmount(fromAmount, swap.hardcodedRate));
   }
   return Result.fail(
     new SwapError(SwapError.reasons.SwapNotHardcoded, fromAmount, fromAssetId, fromChainId, toAssetId, toChainId),
