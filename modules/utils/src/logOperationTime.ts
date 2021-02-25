105a930637217788ace93ca01e9d462925db7cfa
Jake <jcb.kdd@gmail.com>
2021-02-25 12:34:30 -0800

    update logOperationTime to use methodId for mark label

--- modules/utils/src/logOperationTime.ts
+++ modules/utils/src/logOperationTime.ts
@@ -1,20 +1,22 @@
 import pino from "pino";
 import { performance } from 'perf_hooks';
-import { getRandomBytes32 } from "@connext/vector-utils";
 
 /// This method is used to log the start and end time of an executed method. We are using the
 /// perf_hooks (performance) lib provided by node to calculate this - much more accurate than
 /// Date.now(). On first call, a unique label string is generated and returned. On second call,
 /// same label string should be passed in to measure and log the diff in time (i.e. amount of
 /// time method took to execute in ms).
-export const logOperationTime = (log: pino.BaseLogger, method: string, label?: string): string => {
-  if (label) {
-    const stopLabel = `${label}-stop`;
-    const timestamp = performance.mark(stopLabel);
-    const diff = performance.measure(method, label, stopLabel);
-    const message = `${method} : finished @ ${timestamp}; took ${diff} ms`;
+export const logOperationTime = (log: pino.BaseLogger, method: string, methodId: string, startMark?: string): string => {
+  const mark = `${method}-${methodId}`
+  if (startMark) {
+    const stopMark = `${mark}-stop`;
+    const timestamp = performance.mark(stopMark);
+    performance.measure(mark, startMark, stopMark);
+    const diff = performance.getEntries({name : mark, entryType: "measure"});
+
     // Depending on how long method took, up severity of logging call. This is a default
     // arrangement that was copied over from indra.
+    const message = `${method} : finished; took ${diff} ms`;
     if (diff < 2) {
       log.debug(message);
     } else if (diff < 200) {
@@ -24,10 +26,12 @@ export const logOperationTime = (log: pino.BaseLogger, method: string, label?: s
     }
   } else {
     // Generate the label we'll be using (for later ref) using unique hexstring.
-    const label = `${method}-${getRandomBytes32()}`;
-    performance.mark(label);
+    const startMark = `${mark}-start`;
+    performance.mark(startMark);
+    // Debug log notify method started.
     const message = `${method}: started`;
     log.debug(message);
   }
-  return label;
+  // We return the startMark so it can be passed back in for later method call.
+  return startMark;
 };
