5042c0a4870066820eeb0e9a04dc7a075d4d40fd
LayneHaber <layne.haber@gmail.com>
2021-03-01 13:16:29 -0700

    use default values even in swap

--- modules/router/src/services/config.ts
+++ modules/router/src/services/config.ts
@@ -69,13 +69,13 @@ export const getSwapFees = (
   let gasSubsidyPercentage = getConfig().baseGasSubsidyPercentage ?? 100;
   if (fromChainId !== toChainId || fromAssetId !== toAssetId) {
     const swapRes = getMatchingSwap(fromAssetId, fromChainId, toAssetId, toChainId);
-    if (swapRes.isError) {
+    if (swapRes.isError && swapRes.getError()?.message !== ConfigServiceError.reasons.UnableToFindSwap) {
       return Result.fail(swapRes.getError()!);
     }
-    const swap = swapRes.getValue();
-    flatFee = swap.flatFee ?? flatFee;
-    percentageFee = swap.percentageFee ?? percentageFee;
-    gasSubsidyPercentage = swap.gasSubsidyPercentage ?? gasSubsidyPercentage;
+    const swap = swapRes.isError ? undefined : swapRes.getValue();
+    flatFee = swap?.flatFee ?? flatFee;
+    percentageFee = swap?.percentageFee ?? percentageFee;
+    gasSubsidyPercentage = swap?.gasSubsidyPercentage ?? gasSubsidyPercentage;
   }
   return Result.ok({
     flatFee,
