dc6b7ea6fc532a6b7bd1990fe58dc8f379a383db
LayneHaber <layne.haber@gmail.com>
2021-03-05 20:25:26 -0700

    Fix axios logging

--- modules/utils/src/error.ts
+++ modules/utils/src/error.ts
@@ -7,19 +7,21 @@ export const logAxiosError = (
   additionalContext = {},
   message = "Error sending request",
 ): void => {
-  let errorObj = {};
+  let errorObj: any = { message: error.message ?? message };
   if (error.response) {
     // The request was made and the server responded with a status code
     // that falls out of the range of 2xx
-    errorObj = { data: error.response.data, status: error.response.status, headers: error.response.headers };
+    errorObj = {
+      ...errorObj,
+      data: error.response.data,
+      status: error.response.status,
+      headers: error.response.headers,
+    };
   } else if (error.request) {
     // The request was made but no response was received
     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
     // http.ClientRequest in node.js
-    errorObj = { request: "Error in request" };
-  } else {
-    // Something happened in setting up the request that triggered an Error
-    errorObj = { message: error.message };
+    errorObj = { ...errorObj, request: "Error in request" };
   }
   logger.error({ ...errorObj, ...additionalContext, config: error.config }, message);
 };
