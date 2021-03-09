0ebc36ac12e2f84999b6d96452d7a7271512aa80
Rahul Sethuram <rahul@sethuram.com>
2021-03-09 09:08:18 +0000

    Combined

---
+++ modules/contracts/src.sol/transferDefinitions/HashlockTransferAndWithdraw.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.1;
+pragma experimental ABIEncoderV2;
+
+import "./TransferDefinition.sol";
+import "../lib/LibChannelCrypto.sol";
+
+/// @title Withdraw
+/// @author Connext <support@connext.network>
+/// @notice This contract burns the initiator's funds if a mutually signed
+///         withdraw commitment can be generated
+
+contract HashlockTransferAndWithdraw is TransferDefinition {
+    using LibChannelCrypto for bytes32;
+
+    struct TransferState {
+        bytes initiatorSignature;
+        address initiator;
+        address responder;
+        bytes32 data;
+        uint256 nonce; // included so that each withdraw commitment has a unique hash
+        uint256 fee;
+        address callTo;
+        bytes callData;
+        bytes32 lockHash;
+        uint256 expiry; // If 0, then no timelock is enforced
+    }
+
+    struct TransferResolver {
+        bytes responderSignature;
+        bytes32 preImage;
+    }
+
+    // Provide registry information
+    string public constant override Name = "Withdraw";
+    string public constant override StateEncoding =
+        "tuple(bytes initiatorSignature, address initiator, address responder, bytes32 data, uint256 nonce, uint256 fee, address callTo, bytes callData)";
+    string public constant override ResolverEncoding =
+        "tuple(bytes responderSignature)";
+
+    function EncodedCancel() external pure override returns(bytes memory) {
+      TransferResolver memory resolver;
+      resolver.responderSignature = new bytes(65);
+      return abi.encode(resolver);
+    }
+
+    function create(bytes calldata encodedBalance, bytes calldata encodedState)
+        external
+        view
+        override
+        returns (bool)
+    {
+        // Get unencoded information
+        TransferState memory state = abi.decode(encodedState, (TransferState));
+        Balance memory balance = abi.decode(encodedBalance, (Balance));
+
+        require(balance.amount[1] == 0, "HashlockTransferAndWithdraw: NONZERO_RECIPIENT_BALANCE");
+        require(
+            state.initiator != address(0) && state.responder != address(0),
+            "HashlockTransferAndWithdraw: EMPTY_SIGNERS"
+        );
+        require(state.data != bytes32(0), "HashlockTransferAndWithdraw: EMPTY_DATA");
+        require(state.nonce != uint256(0), "HashlockTransferAndWithdraw: EMPTY_NONCE");
+        require(
+            state.fee <= balance.amount[0],
+            "HashlockTransferAndWithdraw: INSUFFICIENT_BALANCE"
+        );
+        require(
+            state.data.checkSignature(
+                state.initiatorSignature,
+                state.initiator
+            ),
+            "HashlockTransferAndWithdraw: INVALID_INITIATOR_SIG"
+        );
+
+        require(
+            balance.amount[0] > 0,
+            "HashlockTransferAndWithdraw: ZER0_SENDER_BALANCE"
+        );
+
+        require(
+            balance.amount[1] == 0,
+            "HashlockTransferAndWithdraw: NONZERO_RECIPIENT_BALANCE"
+        );
+        require(
+            state.lockHash != bytes32(0),
+            "HashlockTransferAndWithdraw: EMPTY_LOCKHASH"
+        );
+        require(
+            state.expiry == 0 || state.expiry > block.timestamp,
+            "HashlockTransferAndWithdraw: EXPIRED_TIMELOCK"
+        );
+        
+        // Valid initial transfer state
+        return true;
+    }
+
+    function resolve(
+        bytes calldata encodedBalance,
+        bytes calldata encodedState,
+        bytes calldata encodedResolver
+    ) external pure override returns (Balance memory) {
+        TransferState memory state = abi.decode(encodedState, (TransferState));
+        TransferResolver memory resolver =
+            abi.decode(encodedResolver, (TransferResolver));
+        Balance memory balance = abi.decode(encodedBalance, (Balance));
+
+        // Allow for a withdrawal to be canceled if an empty signature is 
+        // passed in. Should have *specific* cancellation action, not just
+        // any invalid sig
+        bytes memory b = new bytes(65);
+        if (keccak256(resolver.responderSignature) == keccak256(b)) {
+            // Withdraw should be cancelled, no state manipulation needed
+        } else {
+            require(
+                state.data.checkSignature(
+                    resolver.responderSignature,
+                    state.responder
+                ),
+                "Withdraw: INVALID_RESPONDER_SIG"
+            );
+            // Reduce withdraw amount by optional fee
+            // It's up to the offchain validators to ensure that the withdraw commitment takes this fee into account
+            balance.amount[1] = state.fee;
+            balance.amount[0] = 0;
+        }
+
+        return balance;
+    }
+}
