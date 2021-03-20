e808d81361f3b827668380ccfd57ceee479ccd37
Rahul Sethuram <rahul@sethuram.com>
2021-03-20 10:32:59 +0000

    Batch submitter

---
+++ modules/contracts/src.sol/BatchSubmitter.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.1;
+pragma experimental ABIEncoderV2;
+
+import "./interfaces/ICMCWithdraw.sol";
+import "./interfaces/IChannelFactory.sol";
+
+struct BatchWithdrawData {
+    WithdrawData withdrawData;
+    address alice;
+    address bob;
+    bytes aliceSignature,
+    bytes bobSignature
+}
+
+/// @title BatchSubmitter
+/// @author Connext <support@connext.network>
+/// @notice 
+contract BatchSubmitter {
+    IChannelFactory immutable CHANNEL_FACTORY;
+
+    constructor(address channelFactoryAddress) {
+        CHANNEL_FACTORY = IChannelFactory(channelFactoryAddress);
+    }
+
+    function submitBatchWithdrawals(BatchWithdrawData[] calldata withdrawals)
+        external
+        returns (bool)
+    {
+        for (uint i = 0; i < withdrawals.length; i++) {
+            BatchWithdrawData calldata withdrawal = withdrawals[i];
+            address channelAddress = withdrawal.withdrawData.channelAddress;
+            ICMCWithdraw channel = ICMCWithdraw(withdrawal.withdrawData.channelAddress);
+
+            // check if contract is deployed
+            uint32 size;
+            assembly {
+                size := extcodesize(channelAddress)
+            }
+
+            // needs to be deployed
+            if (size == 0) {
+                CHANNEL_FACTORY.createChannelAndDepositAlice(
+                    withdrawal.alice, 
+                    withdrawal.bob, 
+                    withdrawal.withdrawData.assetId, 
+                    withdrawal.withdrawData.amount
+                );
+            }
+
+            channel.withdraw(withdrawal.withdrawData, withdrawal.aliceSignature, withdrawal.bobSignature);
+        }
+        return true;
+    }
+}
