8bea498a9e990fb160b99bb83f6166ba641edff6
Sanchay Mittal <sanchaymittal@gmail.com>
2021-04-22 21:56:52 +0530

    new: unit test for AMM func

--- modules/router/src/test/services/config.spec.ts
+++ modules/router/src/test/services/config.spec.ts
@@ -1,13 +1,32 @@
+import { VectorChainReader, StableSwap } from "@connext/vector-contracts";
 import { expect, mkAddress } from "@connext/vector-utils";
-
+import { Result } from "@connext/vector-types";
+import Sinon from "sinon";
 import { getConfig } from "../../config";
 import { ConfigServiceError } from "../../errors";
-import { getRebalanceProfile } from "../../services/config";
+import { getRebalanceProfile, getMappedAssets, onSwapGivenIn } from "../../services/config";
+import { parseEther } from "ethers/lib/utils";
 
 const config = getConfig();
 
-describe("config.ts", () => {
+describe.only("config.ts", () => {
   const chainId = parseInt(Object.keys(config.chainProviders)[0]);
+  const transferAmount = parseEther("1");
+  const fromAssetId: string = mkAddress("0xA");
+  const fromChainId: number = 1337;
+  const toAssetId: string = mkAddress("0xB");
+  const toChainId: number = 1338;
+  const routerSignerAddress: string = mkAddress("0xC");
+  let ethReader: Sinon.SinonStubbedInstance<VectorChainReader>;
+
+  beforeEach(async () => {
+    ethReader = Sinon.createStubInstance(VectorChainReader);
+  });
+
+  afterEach(() => {
+    Sinon.restore();
+  });
+
   describe("getRebalanceProfile", () => {
     it("should fail if rebalance profile does not exist", () => {
       const assetId = mkAddress("0xeee");
@@ -24,19 +43,53 @@ describe("config.ts", () => {
     });
   });
 
-  describe("getMatchingSwap", () => {
+  describe("getMatchingSwap", () => {});
 
+  describe("getSwapFees", () => {});
+
+  describe("getMappedAssets", () => {
+    it("happy: getMappedAssets", () => {
+      const res = getMappedAssets(fromAssetId, fromChainId);
+      console.log(res);
+    });
   });
 
-  describe("getSwapFees", () => {
+  describe.skip("onSwapGivenIn", () => {
+    it("error if getOnchainBalance errors", () => {
+      ethReader.getOnchainBalance.onFirstCall().rejects(
+        Result.fail(
+          new ConfigServiceError(ConfigServiceError.reasons.CouldNotGetAssetBalance, {
+            transferAmount,
+            fromAssetId,
+            fromChainId,
+            routerSignerAddress,
+          }),
+        ),
+      );
+      // const res = await onSwapGivenIn(
+      //   transferAmount.toString(),
+      //   fromAssetId,
+      //   fromChainId,
+      //   toAssetId,
+      //   toChainId,
+      //   routerSignerAddress,
+      //   ethReader,
+      // );
+      // expect(res.isError()).to.be.true;
+    });
 
+    it("case", async () => {
+      // const res = await onSwapGivenIn(
+      //   transferAmount.toString(),
+      //   fromAssetId,
+      //   fromChainId,
+      //   toAssetId,
+      //   toChainId,
+      //   routerSignerAddress,
+      //   ethReader,
+      // );
+    });
   });
 
-  describe("onSwapGivenIn", () => {
-    
-  });
-
-  describe("shouldChargeFees", () => {
-
-  });
+  describe("shouldChargeFees", () => {});
 });
