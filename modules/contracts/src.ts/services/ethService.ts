b32d340dc44bf58c0c4b67b7d57c8856168249e1
Jake Kidd <jcb.kdd@gmail.com>
2021-05-04 18:55:30 -0700

    fix : handle result from deploy

--- modules/contracts/src.ts/services/ethService.ts
+++ modules/contracts/src.ts/services/ethService.ts
@@ -1171,7 +1171,7 @@ export class EthereumChainService extends EthereumChainReader implements IVector
           method,
           methodId,
           channelAddress: channelState.channelAddress,
-          transactionHash: deploy.getValue().transactionHash,
+          transactionHash: deploy.getValue()!.transactionHash,
         },
         "Channel deployed",
       );
