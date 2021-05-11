110b3491ad68a41f6ef5a196337b37bd315cb6e0
Jake Kidd <jcb.kdd@gmail.com>
2021-05-10 18:11:33 -0700

    include updated attempts

--- modules/browser-node/src/services/store.ts
+++ modules/browser-node/src/services/store.ts
@@ -371,7 +371,7 @@ export class BrowserStore implements IEngineStore, IChainServiceStore {
       value: response.value.toString(),
       chainId: response.chainId,
       nonce: response.nonce,
-      attempts: [],
+      attempts,
       receipt: undefined,
       confirmedTransactionHash: undefined,
     } as StoredTransaction, onchainTransactionId);
