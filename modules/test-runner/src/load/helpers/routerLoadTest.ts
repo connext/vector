bf1b365091f796736e431c2d3f3bca4fee57159e
Marcus H <c-marcus.hearne@charter.com>
2021-05-17 14:30:34 -0600

    stubbing out and adding logic for router load tests

---
+++ modules/test-runner/src/load/helpers/routerLoadTest.ts
@@ -0,0 +1,128 @@
+import { Wallet, utils, constants } from "ethers";
+import {env, fundIfBelow, getRandomIndex} from "../../utils";
+import { EngineEvents, INodeService, TransferNames } from "@connext/vector-types";
+import {RestServerNodeService} from "@connext/vector-utils";
+import pino from "pino";
+import {carolEvts} from "./setupServer";
+import {
+    advanceBlocktime,
+    chainId1,
+    chainId2,
+    defundChannel,
+    deposit,
+    disputeChannel,
+    exitAssets,
+    provider1,
+    requestCollateral,
+    setup,
+    transfer,
+    wallet1,
+    wallet2,
+    withdraw,
+} from "../../utils/channel";
+const logger = pino({ level: env.logLevel });
+
+const testName:string = 'Router Load Test'
+const rogerURL:string = "localhost?"
+const carolURL:string = "localhost"
+
+
+let routers:RestServerNodeService[] = [];
+let nodes:RestServerNodeService[] = [];
+
+async function setupRoger() {
+    const routerIndex = getRandomIndex();
+    //verify below
+    // const routerIndex = 0;
+    const events = undefined;
+    const min = utils.parseEther("0.1");
+
+
+    const roger = await RestServerNodeService.connect(
+        rogerURL,
+        logger.child({testName, name:"roger"}),
+        events,
+        routerIndex
+    )
+    //fund roger
+    // Default collateral is 0.1 ETH
+    await fundIfBelow(roger.signerAddress, constants.AddressZero, min.mul(15), wallet1);
+    if (wallet2) {
+        await fundIfBelow(roger.signerAddress, constants.AddressZero, min.mul(15), wallet2);
+    }
+
+    return roger
+}
+async function setupNode() {
+    const routerIndex = getRandomIndex();
+    //verify below
+    // const routerIndex = 0;
+    const events = undefined;
+    const min = utils.parseEther("0.1");
+
+
+    const roger = await RestServerNodeService.connect(
+        rogerURL,
+        logger.child({testName, name:"carol"}),
+        carolEvts,
+        routerIndex
+    )
+    //fund roger
+    // Default collateral is 0.1 ETH
+    await fundIfBelow(roger.signerAddress, constants.AddressZero, min.mul(15), wallet1);
+    if (wallet2) {
+        await fundIfBelow(roger.signerAddress, constants.AddressZero, min.mul(15), wallet2);
+    }
+
+    return roger
+}
+async function createRouters(num_routers:number){
+    for(let i = 0; i <= num_routers; i++){
+        const roger = await setupRoger()
+        routers.push(roger);
+    }
+    return routers;
+}
+//non router nodes
+async function createNode(num_nodes:number){
+    for(let i = 0; i <= num_nodes; i++){
+        const carol = await setupNode()
+        nodes.push(carol);
+    }
+    return nodes;
+}
+//get two roters to setup together
+const getRandomEntity = async function(entityArr:RestServerNodeService[], num_entities:number){
+    const randEntities = entityArr.map(x => ({ x, r: Math.random() }))
+        .sort((a, b) => a.r - b.r)
+        .map(a => a.x)
+        .slice(0, num_entities);
+    return randEntities;
+}
+const setupChannelsBetweenNodesAndRouter = async function(nodes:RestServerNodeService[], router:RestServerNodeService) {
+    const assetId = constants.AddressZero;
+    const depositAmt = utils.parseEther("1");
+
+    for (let i = 0; i < nodes.length; i++) {
+        const setup_res = await setup(nodes[i], router[0], chainId1)
+        await deposit(nodes[i], router[0], setup_res.channelAddress, assetId, depositAmt)
+    }
+}
+async function start(){
+
+    const transferAmt = utils.parseEther("0.1");
+
+    const num_routers = 5;
+    const num_nodes = 10;
+
+    await createRouters(num_routers);
+
+    await createNode(num_nodes);
+
+    const randomRouter:RestServerNodeService = await getRandomEntity(routers,1)[0];
+    const randomNodes:RestServerNodeService[] = await getRandomEntity(nodes, 3);
+
+    const res = await setupChannelsBetweenNodesAndRouter(randomNodes, randomRouter)
+
+    //setup channel between all carols and the random node
+}
