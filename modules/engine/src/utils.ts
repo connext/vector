2b7cbfb088ee336adf30c5baafa63c6e3a5239f9
Sanchay Mittal <sanchaymittal@gmail.com>
2021-05-19 01:05:38 +0530

    chg: move withdraw try login to utils

--- modules/engine/src/utils.ts
+++ modules/engine/src/utils.ts
@@ -21,13 +21,25 @@ import {
   TransferDisputedPayload,
   TransferDefundedPayload,
   ConditionalTransferRoutingCompletePayload,
+  ChainAddresses,
+  ChannelRpcMethodsResponsesMap,
+  ChannelRpcMethods,
+  EngineError,
+  FullChannelState,
+  IEngineStore,
+  IVectorChainService,
+  TransferNames,
+  jsonifyError,
 } from "@connext/vector-types";
-import { FeeCalculationError, normalizeFee } from "@connext/vector-utils";
+import { WithdrawCommitment } from "@connext/vector-contracts";
+import { FeeCalculationError, normalizeFee, getRandomBytes32 } from "@connext/vector-utils";
 import { BigNumber } from "@ethersproject/bignumber";
 import { Evt } from "evt";
+import { AddressZero, HashZero } from "@ethersproject/constants";
 import { BaseLogger } from "pino";
 
 import { EngineEvtContainer } from "./index";
+import { RpcError } from "./errors";
 
 export const getEngineEvtContainer = (): EngineEvtContainer => {
   return {
@@ -76,4 +88,106 @@ export function normalizeGasFees(
     logger,
     gasPriceOverride,
   );
+}
+
+export async function withdrawRetryForTrasferId(
+  transferId: string,
+  channel: FullChannelState,
+  store: IEngineStore,
+  chainService: IVectorChainService,
+  logger: BaseLogger,
+  publicIdentifier?: string,
+  commitment?: WithdrawCommitment,
+): Promise<Result<ChannelRpcMethodsResponsesMap[typeof ChannelRpcMethods.chan_withdrawRetry], EngineError>> {
+  let _commitment = commitment;
+  if (!_commitment) {
+    const json = await store.getWithdrawalCommitment(transferId);
+    if (!json) {
+      return Result.fail(
+        new RpcError(RpcError.reasons.WithdrawResolutionFailed, channel.channelAddress, publicIdentifier ?? "", {
+          transferId: transferId,
+        }),
+      );
+    }
+
+    if (json.transactionHash) {
+      return Result.fail(
+        new RpcError(RpcError.reasons.TransactionFound, channel.channelAddress, publicIdentifier ?? "", {
+          transferId: transferId,
+          transactionHash: json.transactionHash,
+        }),
+      );
+    }
+
+    _commitment = await WithdrawCommitment.fromJson(json);
+
+    if (!json.bobSignature || !json.aliceSignature) {
+      return Result.fail(
+        new RpcError(RpcError.reasons.CommitmentSingleSigned, channel.channelAddress, publicIdentifier ?? "", {
+          transferId: transferId,
+        }),
+      );
+    }
+  }
+
+  logger.info({ channelAddress: channel.channelAddress, transferId: transferId }, "Withdraw retry initiated");
+  const transaction = await chainService.sendWithdrawTx(channel, _commitment.getSignedTransaction());
+  if (transaction.isError) {
+    return Result.fail(transaction.getError()!);
+  }
+  const txHash = transaction.getValue().transactionHash;
+  commitment!.addTransaction(txHash);
+  await store.saveWithdrawalCommitment(transferId, _commitment!.toJson());
+
+  return Result.ok({
+    transactionHash: txHash,
+    transferId: transferId,
+    channelAddress: channel.channelAddress,
+  });
+}
+
+export async function submitUnsubmittedWithdrawals(
+  channel: FullChannelState,
+  store: IEngineStore,
+  chainAddresses: ChainAddresses,
+  chainService: IVectorChainService,
+  logger: BaseLogger,
+): Promise<void> {
+  const method = "submitUnsubmittedWithdrawals";
+  const methodId = getRandomBytes32();
+
+  const withdrawInfo = await chainService.getRegisteredTransferByName(
+    TransferNames.Withdraw,
+    chainAddresses[channel.networkContext.chainId].transferRegistryAddress,
+    channel.networkContext.chainId,
+  );
+  if (withdrawInfo.isError) {
+    logger.error(
+      { method, methodId, error: withdrawInfo.getError()?.toJson() },
+      "Error in chainService.getRegisteredTransferByName",
+    );
+  }
+
+  const unsubmitted = await store.getUnsubmittedWithdrawals(channel.channelAddress, withdrawInfo.getValue().definition);
+  for (const u of unsubmitted) {
+    logger.info({ method, methodId, transferId: u.transfer.transferId }, "Submitting unsubmitted withdrawal");
+
+    const commitment = await WithdrawCommitment.fromJson(u.commitment);
+    const res = await withdrawRetryForTrasferId(
+      u.transfer.transferId,
+      channel,
+      store,
+      chainService,
+      logger,
+      "",
+      commitment,
+    );
+
+    if (res.isError) {
+      logger.error(
+        { method, methodId, error: jsonifyError(res.getError()!), commitment: u },
+        "Error submitting unsubmitted withdrawal",
+      );
+    }
+  }
 }
