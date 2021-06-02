92730a3d5cba3d0343b65c623d5a95604ca35b96
LayneHaber <layne.haber@gmail.com>
2021-06-02 15:28:57 -0600

    Make restore appropriate

--- modules/server-node/prisma-sqlite/migrations/20210602212112_add_update_id/migration.sql
+++ modules/server-node/prisma-sqlite/migrations/20210602212112_add_update_id/migration.sql
@@ -1,17 +1,14 @@
 /*
   Warnings:
 
-  - The primary key for the `update` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `merkleProofData` on the `update` table. All the data in the column will be lost.
-  - Added the required column `id` to the `update` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `idSignature` to the `update` table without a default value. This is not possible if the table is not empty.
 
 */
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_update" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "idSignature" TEXT NOT NULL,
+    "id" TEXT,
+    "idSignature" TEXT,
     "channelAddress" TEXT,
     "channelAddressId" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -41,12 +38,14 @@ CREATE TABLE "new_update" (
     "responder" TEXT,
     "transferResolver" TEXT,
     "merkleRoot" TEXT,
+
+    PRIMARY KEY ("channelAddressId", "nonce"),
     FOREIGN KEY ("channelAddress") REFERENCES "channel" ("channelAddress") ON DELETE SET NULL ON UPDATE CASCADE
 );
 INSERT INTO "new_update" ("channelAddress", "channelAddressId", "createdAt", "fromIdentifier", "toIdentifier", "type", "nonce", "amountA", "amountB", "toA", "toB", "assetId", "signatureA", "signatureB", "totalDepositsAlice", "totalDepositsBob", "transferAmountA", "transferAmountB", "transferToA", "transferToB", "transferId", "transferDefinition", "transferTimeout", "transferInitialState", "transferEncodings", "meta", "responder", "transferResolver", "merkleRoot") SELECT "channelAddress", "channelAddressId", "createdAt", "fromIdentifier", "toIdentifier", "type", "nonce", "amountA", "amountB", "toA", "toB", "assetId", "signatureA", "signatureB", "totalDepositsAlice", "totalDepositsBob", "transferAmountA", "transferAmountB", "transferToA", "transferToB", "transferId", "transferDefinition", "transferTimeout", "transferInitialState", "transferEncodings", "meta", "responder", "transferResolver", "merkleRoot" FROM "update";
 DROP TABLE "update";
 ALTER TABLE "new_update" RENAME TO "update";
-CREATE UNIQUE INDEX "update.channelAddressId_nonce_unique" ON "update"("channelAddressId", "nonce");
+CREATE UNIQUE INDEX "update.id_unique" ON "update"("id");
 CREATE UNIQUE INDEX "update_channelAddress_unique" ON "update"("channelAddress");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
